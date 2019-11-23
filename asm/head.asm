2c555882a5d5ef8d1c1033fb3b7f3f4cc2d8d235
toku-sa-n <tokuchan3515@gmail.com>
2019-11-23 15:23:36 +0900

    [Change]Increase screen resolution

--- asm/head.asm
+++ asm/head.asm
@@ -1,144 +1,199 @@
 ; haribote-os boot asm
 ; TAB=4
 
-BOTPAK	EQU		0x00280000		; bootpack�̃��[�h��
-DSKCAC	EQU		0x00100000		; �f�B�X�N�L���b�V���̏ꏊ
-DSKCAC0	EQU		0x00008000		; �f�B�X�N�L���b�V���̏ꏊ�i���A�����[�h�j
+BOTPAK  EQU     0x00280000      ; bootpackのロード先
+DSKCAC  EQU     0x00100000      ; ディスクキャッシュの場所
+DSKCAC0 EQU     0x00008000      ; ディスクキャッシュの場所（リアルモード）
 
-; BOOT_INFO�֌W
-CYLS	EQU		0x0ff0			; �u�[�g�Z�N�^���ݒ肷��
-LEDS	EQU		0x0ff1
-VMODE	EQU		0x0ff2			; �F���Ɋւ�����B���r�b�g�J���[���H
-SCRNX	EQU		0x0ff4			; �𑜓x��X
-SCRNY	EQU		0x0ff6			; �𑜓x��Y
-VRAM	EQU		0x0ff8			; �O���t�B�b�N�o�b�t�@�̊J�n�Ԓn
+; BOOT_INFO関係
+CYLS    EQU     0x0ff0          ; ブートセクタが設定する
+LEDS    EQU     0x0ff1
+VMODE   EQU     0x0ff2          ; 色数に関する情報。何ビットカラーか？
+SCRNX   EQU     0x0ff4          ; 解像度のX
+SCRNY   EQU     0x0ff6          ; 解像度のY
+VRAM    EQU     0x0ff8          ; グラフィックバッファの開始番地
 
-		ORG		0xc200			; ���̃v���O�������ǂ��ɓǂݍ��܂��̂�
+        ORG     0xc200          ; このプログラムがどこに読み込まれるのか
 
-; ��ʃ��[�h��ݒ�
+; If VBE doesn't exist, the resolution will be 320x200
+        MOV     AX,0x9000
+        MOV     ES,AX
+        MOV     DI,0
+        MOV     AX,0x4f00
+        INT     0x10
+        CMP     AX,0x004f
+        JNE     screen_320
 
-		MOV		AL,0x13			; VGA�O���t�B�b�N�X�A320x200x8bit�J���[
-		MOV		AH,0x00
-		INT		0x10
-		MOV		BYTE [VMODE],8	; ��ʃ��[�h����������iC���ꂪ�Q�Ƃ���j
-		MOV		WORD [SCRNX],320
-		MOV		WORD [SCRNY],200
-		MOV		DWORD [VRAM],0x000a0000
+; If the version of VBE is less than 2.0, set the resolution as 320x200
+        MOV     AX,[ES:DI+4]
+        CMP     AX,0x0200
+        JB      screen_320
 
-; �L�[�{�[�h��LED��Ԃ�BIOS�ɋ����Ă��炤
+VBEMODE EQU 0x107
 
-		MOV		AH,0x02
-		INT		0x16 			; keyboard BIOS
-		MOV		[LEDS],AL
+; If VBE mode $VBEMODE is not available, use 320x200
+        MOV     CX,VBEMODE
+        MOV     AX,0x4f01
+        INT     0x10
+        CMP     AX,0x004f
+        JNE     screen_320
 
-; PIC����؂̊��荞�݂��󂯕t���Ȃ��悤�ɂ���
-;	AT�݊��@�̎d�l�ł́APIC�̏�����������Ȃ�A
-;	������CLI�O�ɂ���Ă����Ȃ��ƁA���܂Ƀn���O�A�b�v����
-;	PIC�̏������͂��Ƃł��
+; If the number of color is not 8, use 320x200
+        CMP     BYTE [ES:DI+0x19],8
+        JNE     screen_320
 
-		MOV		AL,0xff
-		OUT		0x21,AL
-		NOP						; OUT���߂�A��������Ƃ��܂������Ȃ��@�킪����炵���̂�
-		OUT		0xa1,AL
+; If palette mode is not used, use 320x200
+        CMP     BYTE [ES:DI+0x1b],4
+        JNE     screen_320
 
-		CLI						; �����CPU���x���ł����荞�݋֎~
+; If the 7th bit of mode attribution is 0, use 320x200
+        MOV     AX,[ES:DI+0x00]
+        AND     AX,0x0080
+        JZ      screen_320
 
-; CPU����1MB�ȏ�̃������ɃA�N�Z�X�ł���悤�ɁAA20GATE��ݒ�
+; 画面モードを設定
 
-		CALL	waitkbdout
-		MOV		AL,0xd1
-		OUT		0x64,AL
-		CALL	waitkbdout
-		MOV		AL,0xdf			; enable A20
-		OUT		0x60,AL
-		CALL	waitkbdout
+        MOV     BX,VBEMODE+0x4000
+        MOV     AX,0x4f02
+        INT     0x10
+        MOV     BYTE [VMODE],8  ; 画面モードをメモする（C言語が参照する）
+        MOV     AX,[ES:DI+0x12]
+        MOV     [SCRNX],AX
+        MOV     AX,[ES:DI+0x14]
+        MOV     [SCRNY],AX
+        MOV     EAX,[ES:DI+0x28]
+        MOV     [VRAM],EAX
+        JMP     keystatus
 
-; �v���e�N�g���[�h�ڍs
+screen_320:
+        MOV     AL,0x13
+        MOV     AH,0x00
+        INT     0x10
+        MOV     BYTE [VMODE],8
+        MOV     WORD [SCRNX],320
+        MOV     WORD [SCRNY],200
+        MOV     DWORD   [VRAM],0x000a0000
 
-		LGDT	[GDTR0]			; �b��GDT��ݒ�
-		MOV		EAX,CR0
-		AND		EAX,0x7fffffff	; bit31��0�ɂ���i�y�[�W���O�֎~�̂��߁j
-		OR		EAX,0x00000001	; bit0��1�ɂ���i�v���e�N�g���[�h�ڍs�̂��߁j
-		MOV		CR0,EAX
-		JMP		pipelineflush
+; DO NOT FOLLOW THE INSTRUCTIONS WRITTEN IN BOOK!
+; SEE https://qiita.com/tatsumack/items/491e47c1a7f0d48fc762
+        MOV     DWORD [VRAM],0xfd000000
+
+keystatus:
+; キーボードのLED状態をBIOSに教えてもらう
+
+        MOV     AH,0x02
+        INT     0x16            ; keyboard BIOS
+        MOV     [LEDS],AL
+
+; PICが一切の割り込みを受け付けないようにする
+;   AT互換機の仕様では、PICの初期化をするなら、
+;   こいつをCLI前にやっておかないと、たまにハングアップする
+;   PICの初期化はあとでやる
+
+        MOV     AL,0xff
+        OUT     0x21,AL
+        NOP                     ; OUT命令を連続させるとうまくいかない機種があるらしいので
+        OUT     0xa1,AL
+
+        CLI                     ; さらにCPUレベルでも割り込み禁止
+
+; CPUから1MB以上のメモリにアクセスできるように、A20GATEを設定
+
+        CALL    waitkbdout
+        MOV     AL,0xd1
+        OUT     0x64,AL
+        CALL    waitkbdout
+        MOV     AL,0xdf         ; enable A20
+        OUT     0x60,AL
+        CALL    waitkbdout
+
+; プロテクトモード移行
+
+        LGDT    [GDTR0]         ; 暫定GDTを設定
+        MOV     EAX,CR0
+        AND     EAX,0x7fffffff  ; bit31を0にする（ページング禁止のため）
+        OR      EAX,0x00000001  ; bit0を1にする（プロテクトモード移行のため）
+        MOV     CR0,EAX
+        JMP     pipelineflush
 pipelineflush:
-		MOV		AX,1*8			;  �ǂݏ����\�Z�O�����g32bit
-		MOV		DS,AX
-		MOV		ES,AX
-		MOV		FS,AX
-		MOV		GS,AX
-		MOV		SS,AX
+        MOV     AX,1*8          ;  読み書き可能セグメント32bit
+        MOV     DS,AX
+        MOV     ES,AX
+        MOV     FS,AX
+        MOV     GS,AX
+        MOV     SS,AX
 
-; bootpack�̓]��
+; bootpackの転送
 
-		MOV		ESI,bootpack	; �]����
-		MOV		EDI,BOTPAK		; �]����
-		MOV		ECX,512*1024/4
-		CALL	memcpy
+        MOV     ESI,bootpack    ; 転送元
+        MOV     EDI,BOTPAK      ; 転送先
+        MOV     ECX,512*1024/4
+        CALL    memcpy
 
-; ���łɃf�B�X�N�f�[�^���{���̈ʒu�֓]��
+; ついでにディスクデータも本来の位置へ転送
 
-; �܂��̓u�[�g�Z�N�^����
+; まずはブートセクタから
 
-		MOV		ESI,0x7c00		; �]����
-		MOV		EDI,DSKCAC		; �]����
-		MOV		ECX,512/4
-		CALL	memcpy
+        MOV     ESI,0x7c00      ; 転送元
+        MOV     EDI,DSKCAC      ; 転送先
+        MOV     ECX,512/4
+        CALL    memcpy
 
-; �c��S��
+; 残り全部
 
-		MOV		ESI,DSKCAC0+512	; �]����
-		MOV		EDI,DSKCAC+512	; �]����
-		MOV		ECX,0
-		MOV		CL,BYTE [CYLS]
-		IMUL	ECX,512*18*2/4	; �V�����_������o�C�g��/4�ɕϊ�
-		SUB		ECX,512/4		; IPL�̕�������������
-		CALL	memcpy
+        MOV     ESI,DSKCAC0+512 ; 転送元
+        MOV     EDI,DSKCAC+512  ; 転送先
+        MOV     ECX,0
+        MOV     CL,BYTE [CYLS]
+        IMUL    ECX,512*18*2/4  ; シリンダ数からバイト数/4に変換
+        SUB     ECX,512/4       ; IPLの分だけ差し引く
+        CALL    memcpy
 
-; asmhead�ł��Ȃ���΂����Ȃ����Ƃ͑S�����I������̂ŁA
-;	���Ƃ�bootpack�ɔC����
+; asmheadでしなければいけないことは全部し終わったので、
+;   あとはbootpackに任せる
 
-; bootpack�̋N��
+; bootpackの起動
 
-		MOV		EBX,BOTPAK
-		MOV		ECX,[EBX+16]
-		ADD		ECX,3			; ECX += 3;
-		SHR		ECX,2			; ECX /= 4;
-		JZ		skip			; �]������ׂ����̂��Ȃ�
-		MOV		ESI,[EBX+20]	; �]����
-		ADD		ESI,EBX
-		MOV		EDI,[EBX+12]	; �]����
-		CALL	memcpy
+        MOV     EBX,BOTPAK
+        MOV     ECX,[EBX+16]
+        ADD     ECX,3           ; ECX += 3;
+        SHR     ECX,2           ; ECX /= 4;
+        JZ      skip            ; 転送するべきものがない
+        MOV     ESI,[EBX+20]    ; 転送元
+        ADD     ESI,EBX
+        MOV     EDI,[EBX+12]    ; 転送先
+        CALL    memcpy
 skip:
-		MOV		ESP,[EBX+12]	; �X�^�b�N�����l
-		JMP		DWORD 2*8:0x0000001b
+        MOV     ESP,[EBX+12]    ; スタック初期値
+        JMP     DWORD 2*8:0x0000001b
 
 waitkbdout:
-		IN		 AL,0x64
-		AND		 AL,0x02
-		JNZ		waitkbdout		; AND�̌��ʂ�0�łȂ����waitkbdout��
-		RET
+        IN       AL,0x64
+        AND      AL,0x02
+        JNZ     waitkbdout      ; ANDの結果が0でなければwaitkbdoutへ
+        RET
 
 memcpy:
-		MOV		EAX,[ESI]
-		ADD		ESI,4
-		MOV		[EDI],EAX
-		ADD		EDI,4
-		SUB		ECX,1
-		JNZ		memcpy			; �����Z�������ʂ�0�łȂ����memcpy��
-		RET
-; memcpy�̓A�h���X�T�C�Y�v���t�B�N�X�����Y��Ȃ���΁A�X�g�����O���߂ł�������
+        MOV     EAX,[ESI]
+        ADD     ESI,4
+        MOV     [EDI],EAX
+        ADD     EDI,4
+        SUB     ECX,1
+        JNZ     memcpy          ; 引き算した結果が0でなければmemcpyへ
+        RET
+; memcpyはアドレスサイズプリフィクスを入れ忘れなければ、ストリング命令でも書ける
 
-		ALIGNB	16, DB 0
+        ALIGNB  16, DB 0
 GDT0:
-		TIMES 8 DB 0				; �k���Z���N�^
-		DW		0xffff,0x0000,0x9200,0x00cf	; �ǂݏ����\�Z�O�����g32bit
-		DW		0xffff,0x0000,0x9a28,0x0047	; ���s�\�Z�O�����g32bit�ibootpack�p�j
+        TIMES 8 DB 0                ; ヌルセレクタ
+        DW      0xffff,0x0000,0x9200,0x00cf ; 読み書き可能セグメント32bit
+        DW      0xffff,0x0000,0x9a28,0x0047 ; 実行可能セグメント32bit（bootpack用）
 
-		DW		0
+        DW      0
 GDTR0:
-		DW		8*3-1
-		DD		GDT0
+        DW      8*3-1
+        DD      GDT0
 
-		ALIGNB	16
+        ALIGNB  16
 bootpack:
+
