c7875192047bb75cbd465af5ff7ed31639ee5c28
toku-sa-n <tokuchan3515@gmail.com>
2019-11-25 17:34:11 +0900

    [Refactoring]Compact multiple lines of asm!

--- src/asm.rs
+++ src/asm.rs
@@ -7,16 +7,16 @@ pub fn hlt() -> () {
 pub fn load_eflags() -> i32 {
     let result: i32;
     unsafe {
-        asm!("PUSHFD"::::"intel");
-        asm!("POP EAX":"={EAX}"(result):::"intel");
+        asm!("PUSHFD
+              POP EAX":"={EAX}"(result):::"intel");
     }
     result
 }
 
 pub fn store_eflags(eflags: i32) -> () {
     unsafe {
-        asm!("PUSH EAX"::"EAX"(eflags)::"intel");
-        asm!("POPFD"::::"intel");
+        asm!("PUSH EAX
+              POPFD"::"EAX"(eflags)::"intel");
     }
 }
 
