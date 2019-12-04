6b13f45e535af6126608e45220863fa5bdf373b9
toku-sa-n <42336339+toku-sa-n@users.noreply.github.com>
2019-12-04 22:07:13 +0900

    [Add]Enable mouse interruption

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -19,6 +19,13 @@ const PIC1_ICW4: i32 = 0x00a1;
 
 const PORT_KEYDATA: i32 = 0x0060;
 
+const PORT_KEY_STATUS: i32 = 0x0064;
+const PORT_KEY_CMD: i32 = 0x0064;
+const KEY_STATUS_SEND_NOT_READY: i32 = 0x02;
+const KEY_CMD_WRITE_MODE: i32 = 0x60;
+const KEY_CMD_MODE: i32 = 0x47;
+const KEY_CMD_SEND_TO_MOUSE: i32 = 0xd4;
+const MOUSE_CMD_ENABLE: i32 = 0xf4;
 lazy_static::lazy_static! {
     pub static ref KEY_QUEUE: spin::Mutex<queue::Queue> = spin::Mutex::new(queue::Queue::new());
 }
@@ -45,6 +52,31 @@ pub fn init_pic() -> () {
 pub fn enable_pic1_keyboard_mouse() -> () {
     asm::out8(PIC0_IMR, 0xf9);
     asm::out8(PIC1_IMR, 0xef);
+
+    init_keyboard();
+    enable_mouse();
+}
+
+fn wait_kbc_sendready() -> () {
+    loop {
+        if asm::in8(PORT_KEY_STATUS) & KEY_STATUS_SEND_NOT_READY == 0 {
+            break;
+        }
+    }
+}
+
+fn init_keyboard() -> () {
+    wait_kbc_sendready();
+    asm::out8(PORT_KEY_CMD, KEY_CMD_WRITE_MODE);
+    wait_kbc_sendready();
+    asm::out8(PORT_KEYDATA, KEY_CMD_MODE);
+}
+
+fn enable_mouse() -> () {
+    wait_kbc_sendready();
+    asm::out8(PORT_KEY_CMD, KEY_CMD_SEND_TO_MOUSE);
+    wait_kbc_sendready();
+    asm::out8(PORT_KEYDATA, MOUSE_CMD_ENABLE);
 }
 
 pub extern "C" fn interrupt_handler_21() -> () {
