2631cad9a2164509a9db10855b2dce76cf19b56f
toku-sa-n <tokusan441@gmail.com>
2020-02-24 09:03:17 +0900

    [Add]Page directory entry for kernel

--- asm/paging.asm
+++ asm/paging.asm
@@ -9,3 +9,13 @@
     MOV             ECX, NUM_ALL_ENTRIES
 
     REP             STOSD
+
+    ; Map kernel
+    ; Add an entry for kernel to page directory
+
+    SIZE_TABLE      EQU 0x1000
+    TABLE_KERNEL    EQU DIR + SIZE_TABLE
+    SIZE_ENTRY      EQU 4
+    DIR_ENTRY_KERNEL    EQU DIR + 0x300 * SIZE_ENTRY
+    PAGE_EXISTS     EQU 1
+    MOV             DWORD[DWORD DIR_ENTRY_KERNEL], TABLE_KERNEL | PAGE_EXISTS
