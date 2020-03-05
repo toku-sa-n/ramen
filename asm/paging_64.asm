63f835de11a80490cc19f985dfb2d75e6cbd6636
toku-sa-n <tokusan441@gmail.com>
2020-03-05 11:16:56 +0900

    [Add]Mapping functions for 64bit paging

--- asm/paging_64.asm
+++ asm/paging_64.asm
@@ -26,3 +26,96 @@
     ; Add a PD entry for below 1MB
     PT_BELOW_1MB    EQU PD_BELOW_1MB + 0x1000
     MOV             DWORD[DWORD PD_BELOW_1MB], PT_BELOW_1MB | PAGE_EXISTS
+
+    ; Functions
+
+map_entries:
+    ; Associate physical memories starting with EAX to page directory entries
+    ; starting with EDI.
+    ; Page table will be created successively from physical address EBX.
+    ; EDX will be used as a temporary register.
+    ;
+    ; EAX: Starting address of physical memories.
+    ; EBX: Starting address of page tables.
+    ; EDI: Starting address of entries of a page directory.
+    ; ECX: Number of bytes to map.
+    PUSH            EBP
+    MOV             EBP, ESP
+
+    ; Number of entries = ECX / (bytes of a page table)
+    ;                   = ECX >> 12
+    SHR             ECX, 12
+
+loop_map_entries:
+    ; The number of entries a 4-level page table contains is 512, not 1024.
+    NUM_PAGE_ENTRIES EQU 512
+    CMP             ECX, NUM_PAGE_ENTRIES
+    JBE             map_remainings
+
+    MOV             EDX, EBX
+    OR              EDX, PAGE_EXISTS
+    MOV             [EDI], EDX
+
+    PUSH            ECX,
+    MOV             ECX, NUM_PAGE_ENTRIES
+
+    PUSH            EDI
+    MOV             EDI, EBX
+    CALL            map_to_single_table
+
+    POP             EDI
+    POP             ECX
+
+    SUB             ECX, NUM_PAGE_ENTRIES
+
+    SIZE_TABLE      EQU 0x1000
+    ADD             EBX, SIZE_TABLE
+
+    ; The size of entry of 4-level paging is 8 bytes, not 4.
+    SIZE_ENTRY      EQU 8
+    ADD             EDI, SIZE_ENTRY
+
+    JMP             loop_map_entries
+
+map_remainings:
+    MOV             EDX, EBX
+    OR              EDX, PAGE_EXISTS
+    MOV             [EDI], EDX
+
+    MOV             EDI, EBX
+    CALL            map_to_single_table
+
+    MOV             ESP, EBP
+    POP             EBP
+    RET
+
+map_to_single_table:
+    ; Map ECX entries to a page table.
+    ; (EDI - (page directory base address)) / 4 + ECX must be less than or equal to 1024.
+    ;
+    ; EAX: Starting address of physical memories.
+    ; ECX: Number of entries to map.
+    ; EDI: Starting address of entries of a page table.
+    ; EDX will be used as a temporary register.
+    PUSH            EBP
+    MOV             EBP, ESP
+
+loop_map_to_single_table:
+    CMP             ECX, 0
+    JBE             end_map_to_single_table
+
+    MOV             EDX, EAX
+    OR              EDX, PAGE_EXISTS
+    MOV             [EDI], EDX
+
+    ADD             EAX, 0x1000
+    ADD             EDI, SIZE_ENTRY
+    DEC             ECX
+
+    JMP             loop_map_to_single_table
+
+end_map_to_single_table:
+    MOV             ESP, EBP
+    POP             EBP
+    RET
+
