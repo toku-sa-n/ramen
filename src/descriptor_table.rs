2516785405798c6eebe3626edbc806e5caa65bc8
toku-sa-n <tokusan441@gmail.com>
2020-03-14 10:57:51 +0900

    [Change]IDT supports 64-bit mode

--- src/descriptor_table.rs
+++ src/descriptor_table.rs
@@ -12,16 +12,20 @@ struct GateDescriptor {
     selector: u16,
     dw_count: u8,
     access_right: u8,
-    offset_high: u16,
+    offset_mid: u16,
+    offset_high: u32,
+    reserved: u32,
 }
 
 impl GateDescriptor {
-    fn set_gate_descriptor(&mut self, offset: u32, selector: u16, access_right: u32) -> () {
+    fn set_gate_descriptor(&mut self, offset: u64, selector: u16, access_right: u32) -> () {
         (*self).offset_low = (offset & 0xFFFF) as u16;
         (*self).selector = selector;
         (*self).dw_count = ((access_right >> 8) & 0xFF) as u8;
         (*self).access_right = (access_right & 0xFF) as u8;
-        (*self).offset_high = ((offset >> 16) & 0xFFFF) as u16;
+        (*self).offset_mid = ((offset >> 16) & 0xFFFF) as u16;
+        (*self).offset_high = ((offset >> 32) & 0xFFFFFFFF) as u32;
+        (*self).reserved = 0;
     }
 }
 
@@ -34,7 +38,8 @@ fn init_idt() -> () {
     let interrupt_descriptor_table: *mut GateDescriptor =
         VIRTUAL_ADDRESS_IDT as *mut GateDescriptor;
 
-    for i in 0..=(LIMIT_INTERRUPT_DESCRIPTOR_TABLE / 8) {
+    const SIZE_IDT_ENTRY: u32 = 16;
+    for i in 0..=(LIMIT_INTERRUPT_DESCRIPTOR_TABLE / SIZE_IDT_ENTRY) {
         unsafe {
             (*interrupt_descriptor_table.offset(i as isize)).set_gate_descriptor(0, 0, 0);
         }
@@ -56,13 +61,13 @@ fn set_interruption() {
         VIRTUAL_ADDRESS_IDT as *mut GateDescriptor;
     unsafe {
         (*interrupt_descriptor_table.offset(0x21)).set_gate_descriptor(
-            interrupt_handler!(interrupt_handler_21) as u32,
-            2 * 8,
+            interrupt_handler!(interrupt_handler_21) as u64,
+            3 * 8,
             ACCESS_RIGHT_IDT,
         );
         (*interrupt_descriptor_table.offset(0x2C)).set_gate_descriptor(
-            interrupt_handler!(interrupt_handler_2c) as u32,
-            2 * 8,
+            interrupt_handler!(interrupt_handler_2c) as u64,
+            3 * 8,
             ACCESS_RIGHT_IDT,
         );
     }
