412f49cc86025a8121bfb20cb23fd372bdd3478a
toku-sa-n <tokusan441@gmail.com>
2020-03-19 17:36:24 +0900

    [Refactoring]Make PrepareFilesystem function

--- boot/bootx64.c
+++ boot/bootx64.c
@@ -3,23 +3,40 @@
 static EFI_GUID kEfiLoadedImageProtocolGuid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
 static EFI_GUID kEfiSimpleFileSystemProtocolGuid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
 
-EFI_STATUS EFIAPI EfiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable)
+// 0x00100000 ~ 0x002FFFFF will be used by OS.
+// (Strictly speaking, the range is much narrower.)
+// Needed page number is 2MB / 4KB = 256 * 2;
+const EFI_PHYSICAL_ADDRESS kNumPagesForOS = 256 * 2;
+
+EFI_STATUS PrepareFilesystem(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable, OUT EFI_FILE_PROTOCOL* efi_file_system)
 {
     EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* efi_fio;
     EFI_LOADED_IMAGE_PROTOCOL* efi_loaded_image_protocol;
 
+    EFI_STATUS return_status = SystemTable->BootServices->HandleProtocol(ImageHandle, &kEfiLoadedImageProtocolGuid, (VOID**)&efi_loaded_image_protocol);
+    if (EFI_ERROR(return_status)) {
+        return return_status;
+    }
+
+    return_status = SystemTable->BootServices->HandleProtocol(efi_loaded_image_protocol->DeviceHandle, &kEfiSimpleFileSystemProtocolGuid, (VOID**)&efi_fio);
+    if (EFI_ERROR(return_status)) {
+        return return_status;
+    }
+
+    return_status = efi_fio->OpenVolume(efi_fio, &efi_file_system);
+    if (EFI_ERROR(return_status)) {
+        return return_status;
+    }
+
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS EFIAPI EfiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable)
+{
     // Prepare a filesystem.
-    if (EFI_ERROR(SystemTable->BootServices->HandleProtocol(ImageHandle, &kEfiLoadedImageProtocolGuid, (VOID**)&efi_loaded_image_protocol))) {
-        return 0;
-    }
-
-    if (EFI_ERROR(SystemTable->BootServices->HandleProtocol(efi_loaded_image_protocol->DeviceHandle, &kEfiSimpleFileSystemProtocolGuid, (VOID**)&efi_fio))) {
-        return 0;
-    }
-
     EFI_FILE_PROTOCOL* efi_file_system = NULL;
 
-    if (EFI_ERROR(efi_fio->OpenVolume(efi_fio, &efi_file_system))) {
+    if (EFI_ERROR(PrepareFilesystem(ImageHandle, SystemTable, efi_file_system))) {
         return 0;
     }
 
