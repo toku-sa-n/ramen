8dae66d164ce01f1d19476192e1aece66932a1c6
toku-sa-n <tokusan441@gmail.com>
2020-03-20 23:18:11 +0900

    [Add]Initialize GOP

--- boot/bootx64.c
+++ boot/bootx64.c
@@ -2,6 +2,8 @@
 
 static EFI_GUID kEfiLoadedImageProtocolGuid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
 static EFI_GUID kEfiSimpleFileSystemProtocolGuid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+static EFI_GUID kEfiEdidDiscoveredProtocolGuid = EFI_EDID_DISCOVERED_PROTOCOL_GUID;
+static EFI_GUID kEfiGraphicsOutputProtocolGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
 
 static EFI_PHYSICAL_ADDRESS kPhysicalAddressOS = 0x00100000;
 
@@ -46,6 +48,84 @@ void Print(IN EFI_SYSTEM_TABLE* SystemTable, IN CHAR16* str)
     }
 }
 
+EFI_STATUS GetPreferredResolution(IN EFI_SYSTEM_TABLE* SystemTable, OUT UINT32* x, OUT UINT32* y)
+{
+    EFI_EDID_DISCOVERED_PROTOCOL* edid;
+    EFI_STATUS return_status = SystemTable->BootServices->LocateProtocol(&kEfiEdidDiscoveredProtocolGuid, NULL, (VOID**)&edid);
+    if (!EFI_ERROR(return_status)) {
+        // See VESA E-EDID manual Table 3.1 and 3.21.
+        *x = ((edid->Edid[58] & 0xF0) << 4) + edid->Edid[56];
+        *y = ((edid->Edid[61] & 0xF0) << 4) + edid->Edid[59];
+    }
+
+    return return_status;
+}
+
+EFI_STATUS CheckGopInfo(EFI_GRAPHICS_OUTPUT_MODE_INFORMATION* info)
+{
+    if (info->PixelFormat != PixelBlueGreenRedReserved8BitPerColor) {
+        return EFI_UNSUPPORTED;
+    }
+
+    // According to UEFI Specification 2.8 Errata A, P.479,
+    // . : Pixel
+    // P : Padding
+    // ..........................................PPPPPPPPPP
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|^^^^^^^^^^
+    //             HorizontalResolution         | Paddings
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    //                    PixelsPerScanLine
+    //
+    // This OS doesn't deal with pixel paddings, so return an error if pixel paddings exist.
+    if (info->HorizontalResolution != info->PixelsPerScanLine) {
+        return EFI_UNSUPPORTED;
+    }
+
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS InitGop(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable, OUT EFI_GRAPHICS_OUTPUT_PROTOCOL** gop)
+{
+    UINT32 preferred_resolution_x = 0, preferred_resolution_y = 0;
+    EFI_STATUS return_status = GetPreferredResolution(SystemTable, &preferred_resolution_x, &preferred_resolution_y);
+    if (EFI_ERROR(return_status)) {
+        Print(SystemTable, (CHAR16*)L"Error: Could not get information from EDID.\n");
+        return return_status;
+    }
+
+    EFI_HANDLE* handle_buffer = NULL;
+
+    // Don't replace handle_count with NULL. It won't work.
+    UINTN handle_count = 0;
+    return_status = SystemTable->BootServices->LocateHandleBuffer(ByProtocol, &kEfiGraphicsOutputProtocolGuid, NULL, &handle_count, &handle_buffer);
+    if (EFI_ERROR(return_status)) {
+        Print(SystemTable, (CHAR16*)L"Error: GOP not found (first)\n");
+        return return_status;
+    }
+
+    return_status = SystemTable->BootServices->OpenProtocol(handle_buffer[0], &kEfiGraphicsOutputProtocolGuid, (VOID**)gop, ImageHandle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+    if (EFI_ERROR(return_status)) {
+        Print(SystemTable, (CHAR16*)L"Error: GOP not found (second)\n");
+        return return_status;
+    }
+
+    Print(SystemTable, (CHAR16*)L"GOP Found.\n");
+
+    for (UINT32 i = 0; i < (*gop)->Mode->Mode; i++) {
+        UINTN size_of_info;
+        EFI_GRAPHICS_OUTPUT_MODE_INFORMATION* info;
+        (*gop)->QueryMode(*gop, i, &size_of_info, &info);
+        if (!EFI_ERROR(CheckGopInfo(info)) && info->HorizontalResolution == preferred_resolution_x && info->VerticalResolution == preferred_resolution_y) {
+            (*gop)->SetMode(*gop, i);
+            Print(SystemTable, (CHAR16*)L"Set GOP.\n");
+            return EFI_SUCCESS;
+        }
+    }
+
+    Print(SystemTable, (CHAR16*)L"Error: Preferred video mode not available\n");
+    return EFI_UNSUPPORTED;
+}
+
 EFI_STATUS EFIAPI EfiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable)
 {
     // Prepare a filesystem.
@@ -61,6 +141,9 @@ EFI_STATUS EFIAPI EfiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* System
         while (1)
             ;
     }
+
+    EFI_GRAPHICS_OUTPUT_PROTOCOL* gop = NULL;
+    InitGop(ImageHandle, SystemTable, &gop);
 
     // Open kernel file.
     EFI_FILE_PROTOCOL* kernel_handle = NULL;
