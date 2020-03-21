6b505cf11e72b2f30421e82fa43cfe0cc3517278
toku-sa-n <tokusan441@gmail.com>
2020-03-22 08:39:24 +0900

    [Add]Read kernel file to memory

--- boot/src/bootx64.cpp
+++ boot/src/bootx64.cpp
@@ -41,6 +41,73 @@ EFI_STATUS AllocateMemoryForOS(IN EFI_SYSTEM_TABLE* SystemTable)
     return SystemTable->BootServices->AllocatePages(AllocateAddress, EfiLoaderData, kNumPagesForOS, &kPhysicalAddressOS);
 }
 
+EFI_STATUS GetFileSize(IN EFI_FILE_PROTOCOL* file_system, IN CHAR16* file_name, OUT EFI_PHYSICAL_ADDRESS* file_size)
+{
+#define RETURN_ON_ERROR(condition)     \
+    do {                               \
+        EFI_STATUS STATUS = condition; \
+        if (EFI_ERROR(STATUS)) {       \
+            return STATUS;             \
+        }                              \
+    } while (0)
+
+    EFI_FILE_PROTOCOL* file_handle = NULL;
+    RETURN_ON_ERROR(file_system->Open(file_system, &file_handle, file_name, EFI_FILE_MODE_READ, 0));
+
+    const UINT64 SET_POSITION_TO_EOF = 0xFFFFFFFFFFFFFFFF;
+    RETURN_ON_ERROR(file_handle->SetPosition(file_handle, SET_POSITION_TO_EOF));
+
+    RETURN_ON_ERROR(file_handle->GetPosition(file_handle, file_size));
+
+    // Close function always succeeds.
+    file_handle->Close(file_handle);
+
+#undef RETURN_ON_ERROR
+
+    return EFI_SUCCESS;
+}
+
+VOID* Malloc(IN EFI_SYSTEM_TABLE* SystemTable, IN EFI_PHYSICAL_ADDRESS n)
+{
+    VOID* result = NULL;
+    EFI_STATUS return_status = SystemTable->BootServices->AllocatePool(EfiLoaderData, n, &result);
+    return EFI_ERROR(return_status) ? NULL : result;
+}
+
+VOID Free(IN EFI_SYSTEM_TABLE* SystemTable, IN VOID* p)
+{
+    if (p) {
+        SystemTable->BootServices->FreePool(p);
+    }
+}
+
+EFI_STATUS ReadFileToMemory(IN EFI_SYSTEM_TABLE* SystemTable, IN EFI_FILE_PROTOCOL* file_system, IN CHAR16* file_name, IN VOID* address)
+{
+
+#define RETURN_ON_ERROR(condition)     \
+    do {                               \
+        EFI_STATUS STATUS = condition; \
+        if (EFI_ERROR(STATUS)) {       \
+            return STATUS;             \
+        }                              \
+    } while (0)
+
+    UINT64 file_size = 0;
+    RETURN_ON_ERROR(GetFileSize(file_system, file_name, &file_size));
+
+    Print(SystemTable, (CHAR16*)L"Got file size.\n");
+    EFI_FILE_PROTOCOL* opened_file = NULL;
+    RETURN_ON_ERROR(file_system->Open(file_system, &opened_file, file_name, EFI_FILE_MODE_READ, 0));
+
+    Print(SystemTable, (CHAR16*)L"Opened file.\n");
+#undef RETURN_ON_ERROR
+
+    EFI_STATUS return_status = opened_file->Read(opened_file, (UINTN*)file_size, address);
+    opened_file->Close(opened_file);
+
+    return return_status;
+}
+
 extern "C" EFI_STATUS EFIAPI EfiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable)
 {
     EFI_FILE_PROTOCOL* efi_file_system = NULL;
@@ -61,6 +128,8 @@ extern "C" EFI_STATUS EFIAPI EfiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TA
     Print(SystemTable, (CHAR16*)L"Initializing GOP...\n");
     EFI_GRAPHICS_OUTPUT_PROTOCOL* gop = NULL;
     LOOP_ON_ERROR(InitGop(ImageHandle, SystemTable, &gop), "Failed to initialize GOP.\n");
+
+    LOOP_ON_ERROR(ReadFileToMemory(SystemTable, efi_file_system, (CHAR16*)L"ramen_os.img", (VOID*)0x00200000), L"Failed to read kernel image.\n");
 
     Print(SystemTable, (CHAR16*)L"Hello World!\n");
     Print(SystemTable, (CHAR16*)L"Make America Great Again!\n");
