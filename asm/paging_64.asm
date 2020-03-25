1d756f61b9c97a7e1163ac79591897aa841c3256
toku-sa-n <tokusan441@gmail.com>
2020-03-25 14:30:01 +0900

    [Add]Memo crash report

--- asm/paging_64.asm
+++ asm/paging_64.asm
@@ -109,6 +109,17 @@
     MOV                  RAX, PML4
     MOV                  CR3, RAX
 
+    ; Changing PML4 address doesn't cause any problems.
+    ; MOV RAX, PML4
+    ; MOV CR3, RAX                  <--- not crash.
+    ;
+    ; Assign a value from memory to a register is OK.
+    ; MOV RAX, QWORD[VRAM_PTR]      <--- not crash.
+    ;
+    ; Set a value to memory is not.
+    ; MOV RAX, 0xFFFFFFFF80200000
+    ; MOV QWORD[VRAM_PTR], RAX      <--- crash.
+
     ; Replace pointer to the physical address of VRAM to the virtual one.
     MOV                  RAX, 0xFFFFFFFF80200000
     MOV                  QWORD[VRAM_PTR], RAX
