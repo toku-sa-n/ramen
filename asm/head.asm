df97db7a7ee6a80e7467d36ca2a9dbba6007bbc1
toku-sa-n <tokusan441@gmail.com>
2020-03-25 14:33:18 +0900

    [Change]Comments

--- asm/head.asm
+++ asm/head.asm
@@ -1,11 +1,7 @@
     [BITS 64]
     ORG      0x500
 
-    ; PICが一切の割り込みを受け付けないようにする
-    ; AT互換機の仕様では、PICの初期化をするなら、
-    ; こいつをCLI前にやっておかないと、たまにハングアップする
-    ; PICの初期化はあとでやる
-
+    ; Disable all interrupts.
     MOV      AL,0xFF
     OUT      0x21,AL
     NOP                                  ; OUT命令を連続させるとうまくいかない機種があるらしいので
@@ -13,11 +9,9 @@
 
     CLI                                  ; さらにCPUレベルでも割り込み禁止
 
-    ; bootpackの起動
-
     %include "paging_64.asm"
 
-    MOV      RSP,0xFFFFFFFF800a1000                ; スタック初期値
+    MOV      RSP,0xFFFFFFFF800a1000
 
     ; JMP 0xFFFFFFFF80000000 can't be executed.
     ; Jump to 64 bit immediate address is not supported.
