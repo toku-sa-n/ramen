3b2e3028cf4f954acbdccc5d800454f697f3a2cd
toku-sa-n <tokusan441@gmail.com>
2020-07-14 11:33:47 +0900

    [Add]Hello world program of uefi

--- bootx64/src/main.rs
+++ bootx64/src/main.rs
@@ -2,19 +2,31 @@
 #![feature(lang_items, start)]
 #![no_main]
 
+#[macro_use]
+extern crate log;
+
 extern crate uefi;
+extern crate uefi_services;
 
 use uefi::prelude::{Boot, Handle, Status, SystemTable};
+use uefi::ResultExt;
+
+fn reset_console(system_table: &SystemTable<Boot>) -> () {
+    system_table
+        .stdout()
+        .reset(false)
+        .expect_success("Failed to reset stdout");
+}
+
+fn initialize_uefi_utilities(system_table: &SystemTable<Boot>) -> () {
+    uefi_services::init(&system_table).expect_success("Failed to initialize_uefi_utilities");
+}
 
 #[start]
+#[no_mangle]
 pub fn efi_main(image: Handle, system_table: SystemTable<Boot>) -> Status {
+    initialize_uefi_utilities(&system_table);
+    reset_console(&system_table);
+    info!("Hello World!");
     loop {}
 }
-
-#[panic_handler]
-fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
-    loop {}
-}
-
-#[lang = "eh_personality"]
-extern "C" fn eh_personality() {}
