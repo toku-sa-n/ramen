891016394553c717e5691031a06f0e567492fa30
toku-sa-n <tokusan441@gmail.com>
2020-07-14 09:48:02 +0900

    [Add]Initial settings for no_std

--- bootx64/src/main.rs
+++ bootx64/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![feature(lang_items)]
+#![no_main]
+
+#[panic_handler]
+fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
+    loop {}
 }
+
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
