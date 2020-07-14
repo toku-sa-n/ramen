c4f22e359d8d968e9b8c49329bf0d4d02948ab51
toku-sa-n <tokusan441@gmail.com>
2020-07-14 20:48:52 +0900

    [Add]Open root directory

--- bootx64/src/main.rs
+++ bootx64/src/main.rs
@@ -9,6 +9,9 @@ extern crate uefi;
 extern crate uefi_services;
 
 use uefi::prelude::{Boot, Handle, Status, SystemTable};
+use uefi::proto::loaded_image;
+use uefi::proto::media::file;
+use uefi::proto::media::fs;
 use uefi::ResultExt;
 
 fn reset_console(system_table: &SystemTable<Boot>) -> () {
@@ -22,11 +25,33 @@ fn initialize_uefi_utilities(system_table: &SystemTable<Boot>) -> () {
     uefi_services::init(&system_table).expect_success("Failed to initialize_uefi_utilities");
 }
 
+fn open_root_dir(image: &Handle, system_table: &SystemTable<Boot>) -> file::Directory {
+    let loaded_image = system_table
+        .boot_services()
+        .handle_protocol::<loaded_image::LoadedImage>(*image)
+        .expect_success("Failed to load image");
+
+    let loaded_image = unsafe { &*loaded_image.get() };
+
+    let simple_file_system = system_table
+        .boot_services()
+        .handle_protocol::<fs::SimpleFileSystem>(loaded_image.device())
+        .expect_success("Failed to prepare simple file system.");
+
+    let simple_file_system = unsafe { &mut *simple_file_system.get() };
+
+    simple_file_system
+        .open_volume()
+        .expect_success("Failed to open volume.")
+}
+
 #[start]
 #[no_mangle]
 pub fn efi_main(image: Handle, system_table: SystemTable<Boot>) -> Status {
     initialize_uefi_utilities(&system_table);
     reset_console(&system_table);
     info!("Hello World!");
+    open_root_dir(&image, &system_table);
+    info!("Opened volume");
     loop {}
 }
