39ca1bd062f819342e9cfb51715d6a05a71a6b48
toku-sa-n <tokusan441@gmail.com>
2020-07-16 13:15:47 +0900

    [Add]Define init_gop

--- bootx64/src/main.rs
+++ bootx64/src/main.rs
@@ -8,9 +8,11 @@ extern crate log;
 extern crate uefi;
 extern crate uefi_services;
 
+use core::mem::MaybeUninit;
 use core::ptr;
 use uefi::prelude::{Boot, Handle, Status, SystemTable};
 use uefi::proto::console::gop;
+use uefi::proto::console::gop::PixelFormat;
 use uefi::proto::loaded_image;
 use uefi::proto::media::file;
 use uefi::proto::media::fs;
@@ -52,30 +54,13 @@ fn malloc<T: Sized>(system_table: &SystemTable<Boot>, num: usize) -> uefi::Resul
     }
 }
 
-fn get_gop<'a>(system_table: &'a SystemTable<Boot>) -> gop::GraphicsOutput<'a> {
-    let search_type = SearchType::from_proto::<gop::GraphicsOutput>();
-
-    let buf_length = get_buf_len_for_locate_handler(system_table, search_type);
-
-    let buf = malloc::<Handle>(system_table, buf_length)
-        .expect_success("Failed to allocate buffer for gop.");
-
-    system_table
-        .boot_services()
-        .locate_handle(
-            search_type,
-            Some(unsafe { core::slice::from_raw_parts_mut(buf, buf_length) }),
-        )
-        .expect_success("Failed to locate gop's handle.");
-
-    let gop_buf = unsafe { ptr::read(buf) };
-
+fn get_gop<'a>(system_table: &'a SystemTable<Boot>) -> &'a mut gop::GraphicsOutput<'a> {
     let gop = system_table
         .boot_services()
-        .handle_protocol::<gop::GraphicsOutput>(gop_buf)
-        .expect_success("Failed to get gop.");
+        .locate_protocol::<gop::GraphicsOutput>()
+        .expect_success("Your computer does not support Graphics Output Protocol!");
 
-    unsafe { ptr::read(gop.get()) }
+    unsafe { &mut *gop.get() }
 }
 
 fn open_root_dir(image: &Handle, system_table: &SystemTable<Boot>) -> file::Directory {
@@ -104,8 +89,53 @@ fn initialize(system_table: &SystemTable<Boot>) -> () {
     info!("Hello World!");
 }
 
+fn is_usable_gop_mode(mode: &gop::ModeInfo) -> bool {
+    if mode.pixel_format() != PixelFormat::BGR {
+        return false;
+    }
+
+    // According to UEFI Specification 2.8 Errata A, P.479,
+    // . : Pixel
+    // P : Padding
+    // ..........................................PPPPPPPPPP
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|^^^^^^^^^^
+    //             HorizontalResolution         | Paddings
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    //                    PixelsPerScanLine
+    //
+    // This OS doesn't deal with pixel paddings, so return an error if pixel paddings exist.
+    let (width, _) = mode.resolution();
+    if width != mode.stride() {
+        return false;
+    }
+
+    true
+}
+
+fn set_resolution(gop: &mut gop::GraphicsOutput) -> () {
+    let mut max_height = 0;
+    let mut max_width = 0;
+    let mut preferred_mode = MaybeUninit::<gop::Mode>::uninit();
+
+    for mode in gop.modes() {
+        let mode = mode.expect("Failed to get gop mode.");
+
+        let (width, height) = mode.info().resolution();
+        if height > max_height && width > max_width && is_usable_gop_mode(&mode.info()) {
+            max_height = height;
+            max_width = width;
+            unsafe { preferred_mode.as_mut_ptr().write(mode) }
+        }
+    }
+
+    gop.set_mode(unsafe { &preferred_mode.assume_init() })
+        .expect_success("Failed to set resolution.");
+
+    info!("width: {} height: {}", max_width, max_height);
+}
+
 fn init_gop(image: &Handle, system_table: &SystemTable<Boot>) -> () {
-    get_gop(system_table);
+    set_resolution(get_gop(system_table));
 }
 
 #[start]
@@ -115,5 +145,6 @@ pub fn efi_main(image: Handle, system_table: SystemTable<Boot>) -> Status {
     open_root_dir(&image, &system_table);
     info!("Opened volume");
     init_gop(&image, &system_table);
+    info!("GOP set.");
     loop {}
 }
