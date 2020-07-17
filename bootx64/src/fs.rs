95b67e2c775c8876811d0c2d39b87f89efa968de
toku-sa-n <tokusan441@gmail.com>
2020-07-17 11:02:16 +0900

    [Add]Define kerne_files

--- bootx64/src/fs.rs
+++ bootx64/src/fs.rs
@@ -3,6 +3,27 @@ use uefi::proto::media::file;
 use uefi::proto::media::fs;
 use uefi::ResultExt;
 
+struct KernelFileInfo {
+    name: &'static str,
+    start_address: usize,
+    bytes: usize,
+}
+
+impl KernelFileInfo {
+    const fn new(name: &'static str, start_address: usize, bytes: usize) -> Self {
+        Self {
+            name,
+            start_address,
+            bytes,
+        }
+    }
+}
+
+const kernel_files: [KernelFileInfo; 2] = [
+    KernelFileInfo::new("head.asm.o", 500, 0x500),
+    KernelFileInfo::new("kernel.bin", 0x200000, 0x20000000),
+];
+
 pub fn open_root_dir(system_table: &SystemTable<Boot>) -> file::Directory {
     let simple_file_system = system_table
         .boot_services()
