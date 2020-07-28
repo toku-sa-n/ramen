9c136efb4d04c58cce8ae2fa31e0b5d397988f9b
toku-sa-n <tokusan441@gmail.com>
2020-07-28 23:46:03 +0900

    [Refactoring]Define init.rs

--- bootx64/src/main.rs
+++ bootx64/src/main.rs
@@ -14,32 +14,13 @@ mod debug;
 mod exit;
 mod fs;
 mod gop;
+mod init;
 mod memory;
 
 use core::slice;
 use exit::BootInfo;
 use uefi::prelude::{Boot, Handle, SystemTable};
 use uefi::table::boot;
-use uefi::ResultExt;
-
-fn reset_console(system_table: &SystemTable<Boot>) -> () {
-    system_table
-        .stdout()
-        .reset(false)
-        .expect_success("Failed to reset stdout");
-}
-
-/// Initialize uefi-rs services. This includes initialization of GlobalAlloc, which enables us to
-/// use Collections defined in alloc module, such as Vec and LinkedList.
-fn initialize_uefi_utilities(system_table: &SystemTable<Boot>) -> () {
-    uefi_services::init(&system_table).expect_success("Failed to initialize_uefi_utilities");
-}
-
-fn initialize(system_table: &SystemTable<Boot>) -> () {
-    initialize_uefi_utilities(&system_table);
-    reset_console(&system_table);
-    info!("Hello World!");
-}
 
 fn terminate_boot_services<'a>(
     image: Handle,
@@ -62,7 +43,7 @@ fn terminate_boot_services<'a>(
 #[start]
 #[no_mangle]
 pub fn efi_main(image: Handle, system_table: SystemTable<Boot>) -> ! {
-    initialize(&system_table);
+    init::uefi(&system_table);
 
     let vram_info = gop::init(&system_table);
 
