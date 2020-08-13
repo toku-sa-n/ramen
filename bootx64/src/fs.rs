c506786917d672ce5d6b926a0839540f90754768
toku-sa-n <tokusan441@gmail.com>
2020-08-13 16:06:11 +0900

    [Change]Read the amount of memory comsuption from kernel header

--- bootx64/src/fs.rs
+++ bootx64/src/fs.rs
@@ -7,20 +7,24 @@ use uefi::proto::media::fs;
 use uefi::table::boot::{AllocateType, MemoryType};
 use uefi::ResultExt;
 
+mod kernel_bytes;
+
 struct KernelFileInfo {
     name: &'static str,
     start_address: usize,
-    bytes: usize,
 }
 
 const BYTES_OF_PAGE: usize = 0x1000;
 
+fn bytes_to_num_of_pages(bytes: usize) -> usize {
+    (bytes + BYTES_OF_PAGE - 1) / BYTES_OF_PAGE
+}
+
 impl KernelFileInfo {
-    const fn new(name: &'static str, start_address: usize, bytes: usize) -> Self {
+    const fn new(name: &'static str, start_address: usize) -> Self {
         Self {
             name,
             start_address,
-            bytes,
         }
     }
 
@@ -31,16 +35,12 @@ impl KernelFileInfo {
     fn address(&self) -> usize {
         self.start_address
     }
-
-    fn num_of_pages(&self) -> usize {
-        (self.bytes + BYTES_OF_PAGE - 1) / BYTES_OF_PAGE
-    }
 }
 
 // Using the size of binary as the memory consumption is useless because the size of .bss section
 // is not included in the binary size. Using ELF file may improve effeciency as it might contain
 // the size of memory comsuption.
-const KERNEL_FILE: KernelFileInfo = KernelFileInfo::new("kernel.bin", 0x200000, 0x200000);
+const KERNEL_FILE: KernelFileInfo = KernelFileInfo::new("kernel.bin", 0x200000);
 
 pub fn place_kernel(system_table: &SystemTable<Boot>) -> () {
     let mut root_dir = open_root_dir(system_table);
@@ -62,10 +62,11 @@ fn open_root_dir(system_table: &SystemTable<Boot>) -> file::Directory {
 }
 
 fn open_kernel(system_table: &SystemTable<Boot>, root_dir: &mut file::Directory) -> () {
+    let kernel_bytes = kernel_bytes::get(root_dir);
     let mut kernel_handler = get_kernel_handler(root_dir);
 
-    allocate_for_kernel_file(system_table);
-    read_kernel_on_memory(&mut kernel_handler);
+    allocate_for_kernel_file(system_table, kernel_bytes);
+    read_kernel_on_memory(&mut kernel_handler, kernel_bytes);
 }
 
 fn get_kernel_handler(root_dir: &mut file::Directory) -> file::RegularFile {
@@ -80,29 +81,26 @@ fn get_kernel_handler(root_dir: &mut file::Directory) -> file::RegularFile {
     unsafe { file::RegularFile::new(handler) }
 }
 
-fn allocate_for_kernel_file(system_table: &SystemTable<Boot>) -> () {
+fn allocate_for_kernel_file(system_table: &SystemTable<Boot>, kernel_bytes: usize) -> () {
     system_table
         .boot_services()
         .allocate_pages(
             AllocateType::Address(KERNEL_FILE.address()),
             MemoryType::LOADER_DATA,
-            KERNEL_FILE.num_of_pages(),
+            bytes_to_num_of_pages(kernel_bytes),
         )
         .expect_success("Failed to allocate memory for the kernel");
 
     // It is not necessary to return the address as it is fixed.
 }
 
-fn read_kernel_on_memory(handler: &mut file::RegularFile) -> () {
+fn read_kernel_on_memory(handler: &mut file::RegularFile, kernel_bytes: usize) -> () {
     // Reading should use while statement with the number of bytes which were actually read.
     // However, without while statement previous uefi implementation worked so this uefi
     // implementation also never use it.
     handler
         .read(unsafe {
-            core::slice::from_raw_parts_mut(
-                KERNEL_FILE.address() as *mut u8,
-                KERNEL_FILE.num_of_pages() * BYTES_OF_PAGE,
-            )
+            core::slice::from_raw_parts_mut(KERNEL_FILE.address() as *mut u8, kernel_bytes)
         })
         .expect_success("Failed to read kernel");
 }
---
+++ bootx64/src/fs/kernel_bytes.rs
@@ -0,0 +1,33 @@
+use core::ptr;
+use uefi::proto::media::file;
+
+struct KernelHeader {
+    _entry_addr: usize,
+    memory_bytes: usize,
+}
+
+impl KernelHeader {
+    fn new_from_slice(slice: &[u8; 16]) -> Self {
+        unsafe {
+            Self {
+                _entry_addr: ptr::read(slice.as_ptr() as *const _),
+                memory_bytes: ptr::read(slice.as_ptr().offset(8) as *const _),
+            }
+        }
+    }
+}
+
+pub fn get(root_dir: &mut file::Directory) -> usize {
+    let mut handler = super::get_kernel_handler(root_dir);
+
+    let mut header = [0u8; 16];
+
+    handler
+        .read(&mut header)
+        .expect("Failed to read kernel header")
+        .unwrap();
+
+    let header = KernelHeader::new_from_slice(&header);
+
+    header.memory_bytes
+}
