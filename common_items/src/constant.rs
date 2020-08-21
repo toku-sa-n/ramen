7aa66589d55662d2bdef713718e3d38884054b3c
toku-sa-n <tokusan441@gmail.com>
2020-08-21 15:30:38 +0900

    [Add]VRAM_ADDR as const

--- common_items/src/constant.rs
+++ common_items/src/constant.rs
@@ -1,3 +1,4 @@
 use crate::x86_64::VirtAddr;
 
 pub const KERNEL_ADDR: VirtAddr = VirtAddr::new_truncate(0xffff_ffff_8000_0000);
+pub const VRAM_ADDR: VirtAddr = VirtAddr::new_truncate(0xffff_ffff_a000_0000);
