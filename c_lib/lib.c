4fb3c141dede60981433f550c3ce29cb656c4801
toku-sa-n <tokusan441@gmail.com>
2020-08-26 12:50:19 +0900

    [Change]Rewrite all mem* libs in C
    Speed didn't change.

--- c_lib/lib.c
+++ c_lib/lib.c
@@ -1,6 +1,22 @@
 #include <stddef.h>
 
-extern void* memcpy(void* dest, const void* src, size_t n);
+void* memset(void* dest, int c, size_t n)
+{
+    for (size_t i = 0; i < n; i++) {
+        ((char*)dest)[i] = (char)c;
+    }
+
+    return dest;
+}
+
+void* memcpy(void* dest, const void* src, size_t n)
+{
+    for (size_t i = 0; i < n; i++) {
+        ((char*)dest)[i] = ((char*)src)[i];
+    }
+
+    return dest;
+}
 
 void* memmove(void* dst, const void* src, size_t n)
 {
