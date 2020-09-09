fbbe4657ac5acdfdaa2f2f2bbc73c856deff002a
toku-sa-n <tokusan441@gmail.com>
2020-09-09 17:11:39 +0900

    [Add]Define `Layer`

--- kernel/src/graphics/screen/layer.rs
+++ kernel/src/graphics/screen/layer.rs
@@ -1,1 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use super::{Coord, TwoDimensionalVec};
+use alloc::vec::Vec;
+
+struct Layer {
+    buf: Vec<Vec<u8>>,
+    top_left: Coord<usize>,
+    len: TwoDimensionalVec<usize>,
+}
