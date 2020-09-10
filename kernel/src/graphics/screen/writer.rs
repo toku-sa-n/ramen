8609d78086e19319959e4240d132296a033f5288
toku-sa-n <tokusan441@gmail.com>
2020-09-10 14:30:04 +0900

    [Refactoring]Methodize `print_str`

--- kernel/src/graphics/screen/writer.rs
+++ kernel/src/graphics/screen/writer.rs
@@ -12,41 +12,41 @@ impl Writer {
     pub const fn new(coord: Coord<isize>, color: RGB) -> Self {
         Self { coord, color }
     }
+
+    fn print_str(&mut self, str: &str) {
+        let mut char_x_pos = self.coord.x;
+        let mut char_y_pos = self.coord.y;
+        for c in str.chars() {
+            if c == '\n' {
+                char_x_pos = 0;
+                char_y_pos += isize::try_from(font::FONT_HEIGHT).unwrap();
+                continue;
+            }
+
+            print_char(
+                &Coord::new(char_x_pos, char_y_pos),
+                self.color,
+                font::FONTS[c as usize],
+            );
+            char_x_pos += isize::try_from(font::FONT_WIDTH).unwrap();
+
+            if char_x_pos + isize::try_from(font::FONT_WIDTH).unwrap()
+                >= isize::try_from(Vram::resolution().x).unwrap()
+            {
+                char_x_pos = 0;
+                char_y_pos += isize::try_from(font::FONT_HEIGHT).unwrap();
+            }
+        }
+    }
 }
 
 impl core::fmt::Write for Writer {
     fn write_str(&mut self, s: &str) -> Result<(), core::fmt::Error> {
-        print_str(&self.coord, self.color, s);
+        self.print_str(s);
         self.coord.x += isize::try_from(s.len() * font::FONT_WIDTH).unwrap();
         self.coord.y += self.coord.x / isize::try_from(Vram::resolution().x).unwrap();
         self.coord.x %= isize::try_from(Vram::resolution().x).unwrap();
         Ok(())
-    }
-}
-
-fn print_str(coord: &Coord<isize>, color: RGB, str: &str) {
-    let mut char_x_pos = coord.x;
-    let mut char_y_pos = coord.y;
-    for c in str.chars() {
-        if c == '\n' {
-            char_x_pos = 0;
-            char_y_pos += isize::try_from(font::FONT_HEIGHT).unwrap();
-            continue;
-        }
-
-        print_char(
-            &Coord::new(char_x_pos, char_y_pos),
-            color,
-            font::FONTS[c as usize],
-        );
-        char_x_pos += isize::try_from(font::FONT_WIDTH).unwrap();
-
-        if char_x_pos + isize::try_from(font::FONT_WIDTH).unwrap()
-            >= isize::try_from(Vram::resolution().x).unwrap()
-        {
-            char_x_pos = 0;
-            char_y_pos += isize::try_from(font::FONT_HEIGHT).unwrap();
-        }
     }
 }
 
