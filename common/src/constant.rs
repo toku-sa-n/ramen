59292766df87cc14e7f7a78e92b45d681dc1b550
toku-sa-n <tokusan441@gmail.com>
2020-09-10 17:17:21 +0900

    [Add]`FREE_PAGE_ADDR`

--- common/src/constant.rs
+++ common/src/constant.rs
@@ -6,7 +6,8 @@ use x86_64::structures::paging::{PageSize, Size4KiB};
 
 pub const KERNEL_ADDR: VirtAddr = VirtAddr::new_truncate(0xffff_ffff_8000_0000);
 pub const KERNEL_HEAP_ADDR: VirtAddr = VirtAddr::new_truncate(0xffff_ffff_9000_0000);
-pub const VRAM_ADDR: VirtAddr = VirtAddr::new_truncate(0xffff_ffff_a000_0000);
+pub const FREE_PAGE_ADDR: VirtAddr = VirtAddr::new_truncate(0xffff_ffff_a000_0000); // Used by physical frame manager
+pub const VRAM_ADDR: VirtAddr = VirtAddr::new_truncate(0xffff_ffff_a000_1000);
 pub const STACK_BASE: VirtAddr = VirtAddr::new_truncate(0xffff_ffff_c000_0000);
 pub const STACK_LOWER: VirtAddr =
     VirtAddr::new_truncate(STACK_BASE.as_u64() - NUM_OF_PAGES_STACK.as_bytes().as_usize() as u64);
