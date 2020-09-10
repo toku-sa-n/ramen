6f9c9c9f90a65e95a7855c084739c6702d20fd7f
toku-sa-n <tokusan441@gmail.com>
2020-09-10 16:02:47 +0900

    [Refactoring]Remove unnecessary variables

--- kernel/src/graphics/screen/writer.rs
+++ kernel/src/graphics/screen/writer.rs
@@ -16,27 +16,21 @@ impl Writer {
     }
 
     fn print_str(&mut self, str: &str) {
-        let mut char_x_pos = self.coord.x;
-        let mut char_y_pos = self.coord.y;
         for c in str.chars() {
             if c == '\n' {
-                char_x_pos = 0;
-                char_y_pos += isize::try_from(font::FONT_HEIGHT).unwrap();
+                self.coord.x = 0;
+                self.coord.y += isize::try_from(font::FONT_HEIGHT).unwrap();
                 continue;
             }
 
-            print_char(
-                &Vec2::new(char_x_pos, char_y_pos),
-                self.color,
-                font::FONTS[c as usize],
-            );
-            char_x_pos += isize::try_from(font::FONT_WIDTH).unwrap();
+            print_char(&self.coord, self.color, font::FONTS[c as usize]);
+            self.coord.x += isize::try_from(font::FONT_WIDTH).unwrap();
 
-            if char_x_pos + isize::try_from(font::FONT_WIDTH).unwrap()
+            if self.coord.x + isize::try_from(font::FONT_WIDTH).unwrap()
                 >= isize::try_from(Vram::resolution().x).unwrap()
             {
-                char_x_pos = 0;
-                char_y_pos += isize::try_from(font::FONT_HEIGHT).unwrap();
+                self.coord.x = 0;
+                self.coord.y += isize::try_from(font::FONT_HEIGHT).unwrap();
             }
         }
     }
@@ -45,9 +39,6 @@ impl Writer {
 impl core::fmt::Write for Writer {
     fn write_str(&mut self, s: &str) -> Result<(), core::fmt::Error> {
         self.print_str(s);
-        self.coord.x += isize::try_from(s.len() * font::FONT_WIDTH).unwrap();
-        self.coord.y += self.coord.x / isize::try_from(Vram::resolution().x).unwrap();
-        self.coord.x %= isize::try_from(Vram::resolution().x).unwrap();
         Ok(())
     }
 }
