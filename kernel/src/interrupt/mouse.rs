3fea47f7b83c14620430d5cf9f023ce0998b75a4
toku-sa-n <tokusan441@gmail.com>
2020-09-12 23:58:13 +0900

    [Refactoring]Remove unnecessary files

--- kernel/src/interrupt/mouse.rs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-use crate::graphics::screen::Screen;
-use alloc::collections::vec_deque::VecDeque;
-use conquer_once::spin::Lazy;
-use rgb::RGB8;
-use spinning_top::Spinlock;
-use vek::Vec2;
-use x86_64::instructions::port::Port;
