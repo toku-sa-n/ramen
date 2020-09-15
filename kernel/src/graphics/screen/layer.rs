db82298481ab6ff1083c4c1189ac1c48136bedbf
toku-sa-n <tokusan441@gmail.com>
2020-09-15 16:24:30 +0900

    [Refactoring]Introduce `layer_bottom_right`

--- kernel/src/graphics/screen/layer.rs
+++ kernel/src/graphics/screen/layer.rs
@@ -53,8 +53,10 @@ impl Controller {
 
     fn repaint(&self, vram_top_left: Vec2<i32>, len: Vec2<i32>) {
         for layer in &self.0 {
-            for y in layer.top_left.y..(layer.top_left.y + layer.len.y) {
-                for x in layer.top_left.x..(layer.top_left.x + layer.len.x) {
+            let layer_bottom_right = layer.top_left + layer.len;
+
+            for y in layer.top_left.y..layer_bottom_right.y {
+                for x in layer.top_left.x..layer_bottom_right.x {
                     if let Some(rgb) = layer.buf[usize::try_from(y - layer.top_left.y).unwrap()]
                         [usize::try_from(x - layer.top_left.x).unwrap()]
                     {
