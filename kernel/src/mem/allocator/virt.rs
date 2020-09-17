c1a56f75da35f6c6c970ec4bbecb9cc38fb681a1
toku-sa-n <tokusan441@gmail.com>
2020-09-17 16:59:21 +0900

    Define `search_first_unused_page`
    To allocate virtual address for xhci and pci configuration.

--- kernel/src/mem/allocator/virt.rs
+++ kernel/src/mem/allocator/virt.rs
@@ -1,1 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use {
+    crate::mem::paging::pml4::PML4,
+    common::constant::LIMIT_VIRT_ADDR,
+    x86_64::{
+        structures::paging::{MapperAllSizes, Page, PageSize, Size4KiB},
+        VirtAddr,
+    },
+};
+
+pub fn search_first_unused_page() -> Option<Page> {
+    for addr in (0..LIMIT_VIRT_ADDR.as_u64()).step_by(Size4KiB::SIZE as _) {
+        let virt_addr = VirtAddr::new(addr);
+        if PML4.lock().translate_addr(virt_addr).is_none() {
+            return Some(Page::containing_address(virt_addr));
+        }
+    }
+    None
+}
