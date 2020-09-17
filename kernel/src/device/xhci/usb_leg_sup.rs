1abf8549aadd5ca139f37f1e5d4b3bd04d894ba0
toku-sa-n <tokusan441@gmail.com>
2020-09-17 17:22:15 +0900

    Define prototypes

--- kernel/src/device/xhci/usb_leg_sup.rs
+++ kernel/src/device/xhci/usb_leg_sup.rs
@@ -1,1 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+struct UsbLegacySupportCapabilityRegister;
+
+impl UsbLegacySupportCapabilityRegister {
+    fn get_hc_bios_owned_semaphore() -> bool {}
+    fn get_hc_os_owned_semaphore() -> bool {}
+    fn set_hc_os_owned_semaphore() {}
+}
