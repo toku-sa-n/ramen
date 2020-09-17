495d750e7ef093e80ac9d83339e052dff9c37995
toku-sa-n <tokusan441@gmail.com>
2020-09-17 17:01:16 +0900

    Clippy

--- kernel/src/mem/allocator/virt.rs
+++ kernel/src/mem/allocator/virt.rs
@@ -3,6 +3,7 @@
 use {
     crate::mem::paging::pml4::PML4,
     common::constant::LIMIT_VIRT_ADDR,
+    core::convert::TryFrom,
     x86_64::{
         structures::paging::{MapperAllSizes, Page, PageSize, Size4KiB},
         VirtAddr,
@@ -10,7 +11,7 @@ use {
 };
 
 pub fn search_first_unused_page() -> Option<Page> {
-    for addr in (0..LIMIT_VIRT_ADDR.as_u64()).step_by(Size4KiB::SIZE as _) {
+    for addr in (0..LIMIT_VIRT_ADDR.as_u64()).step_by(usize::try_from(Size4KiB::SIZE).unwrap()) {
         let virt_addr = VirtAddr::new(addr);
         if PML4.lock().translate_addr(virt_addr).is_none() {
             return Some(Page::containing_address(virt_addr));
