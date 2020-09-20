9c18793e6d0949c342719dd3581e18a1e4ab5de9
toku-sa-n <tokusan441@gmail.com>
2020-09-20 23:52:08 +0900

    Avoid using null address

--- kernel/src/mem/allocator/virt.rs
+++ kernel/src/mem/allocator/virt.rs
@@ -11,11 +11,13 @@ use {
 };
 
 pub fn search_first_unused_page() -> Option<Page> {
+    info!("Searching free virtual address...");
     for addr in
         (0..BYTES_AVAILABLE_RAM.as_usize()).step_by(usize::try_from(Size4KiB::SIZE).unwrap())
     {
         let virt_addr = VirtAddr::new(addr as _);
-        if PML4.lock().translate_addr(virt_addr).is_none() {
+        if PML4.lock().translate_addr(virt_addr).is_none() && virt_addr != VirtAddr::zero() {
+            info!("Found: {:?}", virt_addr);
             return Some(Page::containing_address(virt_addr));
         }
     }
