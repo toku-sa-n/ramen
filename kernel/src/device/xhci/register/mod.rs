939bf47c01c9168bd57016fa7eaab74c4d7b4cb5
toku-sa-n <tokusan441@gmail.com>
2020-09-21 19:13:54 +0900

    Define new method

--- kernel/src/device/xhci/register/mod.rs
+++ kernel/src/device/xhci/register/mod.rs
@@ -5,11 +5,20 @@ pub mod hc_operational_registers;
 pub mod usb_legacy_support_capability;
 
 use {
+    crate::mem::{
+        allocator::{phys::FRAME_MANAGER, virt},
+        paging::pml4::PML4,
+    },
     core::{
         marker::PhantomData,
+        mem::size_of,
         ops::{Deref, DerefMut},
     },
-    x86_64::VirtAddr,
+    os_units::Size,
+    x86_64::{
+        structures::paging::{Mapper, Page, PageSize, PageTableFlags, PhysFrame, Size4KiB},
+        PhysAddr, VirtAddr,
+    },
 };
 
 pub trait Register {
@@ -20,6 +29,49 @@ pub trait Register {
 struct Accessor<'a, T: 'a + Register> {
     base: VirtAddr,
     _marker: PhantomData<&'a T>,
+}
+
+impl<'a, T: 'a + Register> Accessor<'a, T> {
+    pub fn new(phys_base: PhysAddr, offset: usize) -> Self {
+        let phys_base = phys_base + offset;
+
+        let base = Self::map_pages(phys_base);
+
+        Self {
+            base,
+            _marker: PhantomData,
+        }
+    }
+
+    fn map_pages(start: PhysAddr) -> VirtAddr {
+        let start_frame_addr = start.align_down(Size4KiB::SIZE);
+        let end_frame_addr = (start + size_of::<T>()).align_down(Size4KiB::SIZE);
+
+        let num_pages =
+            Size::new((end_frame_addr - start_frame_addr) as _).as_num_of_pages::<Size4KiB>();
+
+        let virt = virt::search_free_addr(num_pages)
+            .expect("OOM during creating a new accessor to a register.");
+
+        for i in 0..num_pages.as_usize() {
+            let page = Page::<Size4KiB>::containing_address(virt + Size4KiB::SIZE * i as u64);
+            let frame = PhysFrame::containing_address(start_frame_addr + Size4KiB::SIZE * i as u64);
+
+            unsafe {
+                PML4.lock()
+                    .map_to(
+                        page,
+                        frame,
+                        PageTableFlags::PRESENT,
+                        &mut *FRAME_MANAGER.lock(),
+                    )
+                    .unwrap()
+                    .flush()
+            }
+        }
+
+        virt
+    }
 }
 
 impl<'a, T: 'a + Register> Deref for Accessor<'a, T> {
