a11a79acb6840a2c3b968052de6cef65b0a8eb76
toku-sa-n <tokusan441@gmail.com>
2020-09-21 18:49:28 +0900

    Define search_free_addr

--- kernel/src/mem/allocator/virt.rs
+++ kernel/src/mem/allocator/virt.rs
@@ -4,6 +4,7 @@ use {
     crate::mem::paging::pml4::PML4,
     common::constant::BYTES_AVAILABLE_RAM,
     core::convert::TryFrom,
+    os_units::{NumOfPages, Size},
     x86_64::{
         structures::paging::{MapperAllSizes, Page, PageSize, Size4KiB},
         VirtAddr,
@@ -21,6 +22,32 @@ pub fn search_first_unused_page() -> Option<Page> {
             return Some(Page::containing_address(virt_addr));
         }
     }
+    None
+}
+
+pub fn search_free_addr(num_pages: Size<NumOfPages<Size4KiB>>) -> Option<VirtAddr> {
+    let mut cnt = 0;
+    let mut start = None;
+    for addr in
+        (0..BYTES_AVAILABLE_RAM.as_usize()).step_by(usize::try_from(Size4KiB::SIZE).unwrap())
+    {
+        let addr = VirtAddr::new(addr as _);
+        if available(addr) {
+            if start.is_none() {
+                start = Some(addr);
+            }
+
+            cnt += 1;
+
+            if cnt >= num_pages.as_usize() {
+                return start;
+            }
+        } else {
+            cnt = 0;
+            start = None;
+        }
+    }
+
     None
 }
 
