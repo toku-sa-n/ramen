eea05a230db0ffc656dddee3e5b3c372756f99ea
toku-sa-n <tokusan441@gmail.com>
2020-09-23 12:50:01 +0900

    Define MessageAddress

--- kernel/src/device/pci/config/msi_x.rs
+++ kernel/src/device/pci/config/msi_x.rs
@@ -8,3 +8,11 @@ bitfield! {
     capability_id, _: 7, 0;
     table_size, _: 25, 16;
 }
+
+bitfield! {
+    struct MessageAddress(u64);
+    u32;
+    destination_id, set_destination_id: 19, 12;
+    redirection_hint, set_redirection_hint: 3;
+    destination_mode, _: 2;
+}
