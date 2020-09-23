75a817f377a7c3385b964192a646fd92574d6fdc
toku-sa-n <tokusan441@gmail.com>
2020-09-23 11:59:56 +0900

    Define struct MsiX

--- kernel/src/device/pci/config/msi_x.rs
+++ kernel/src/device/pci/config/msi_x.rs
@@ -1,1 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use bitfield::bitfield;
+
+bitfield! {
+    pub struct MsiX([u8]);
+    u32;
+    capability_id, _: 7, 0;
+    table_size, _: 25, 16;
+}
