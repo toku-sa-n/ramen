7f1ff03a879704c2df3b22696cff393284e33c04
toku-sa-n <tokusan441@gmail.com>
2020-09-24 10:17:32 +0900

    Define `EndPoint::fetch`

--- kernel/src/device/pci/config/endpoint.rs
+++ kernel/src/device/pci/config/endpoint.rs
@@ -1,1 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use super::{Bar, BarIndex, Bus, Device};
+
+struct EndPoint {
+    bar: [Bar; 6],
+}
+
+impl EndPoint {
+    fn fetch(bus: Bus, device: Device) -> Self {
+        let bar: [Bar; 6];
+        for i in 0..6u32 {
+            bar[i as usize] = Bar::fetch(bus, device, BarIndex::new(i));
+        }
+
+        Self { bar }
+    }
+}
