b554aed8a0bafd7fae0748544ef075e8b0e7dc84
toku-sa-n <tokusan441@gmail.com>
2020-09-24 18:13:16 +0900

    Define `ExtendedCapability`

--- kernel/src/device/extended_capability.rs
+++ kernel/src/device/extended_capability.rs
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-struct ExtendedCapability {}
+#[repr(C, packed)]
+struct ExtendedCapability {
+    capability_id: u8,
+    next_ptr: u8,
+    capability_specific: CapabilitySpecific,
+}
+
+enum CapabilitySpecific {}
