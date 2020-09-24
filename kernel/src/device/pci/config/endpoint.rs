2d939cc14e0937c720559eb6558292a512fca881
toku-sa-n <tokusan441@gmail.com>
2020-09-24 10:56:00 +0900

    Define `EndPoint::base_addr`

--- kernel/src/device/pci/config/endpoint.rs
+++ kernel/src/device/pci/config/endpoint.rs
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use super::{Bar, BarIndex, Bus, Device};
+use {
+    super::{Bar, BarIndex, BarType, Bus, Device},
+    x86_64::PhysAddr,
+};
 
 #[derive(Debug)]
 pub(super) struct EndPoint {
@@ -15,5 +18,22 @@ impl EndPoint {
         }
 
         Self { bar }
+    }
+
+    pub(super) fn base_addr(&self, index: BarIndex) -> PhysAddr {
+        let index = index.as_u32() as usize;
+
+        if index == 5 && self.bar[index].ty() == BarType::Bar64Bit {
+            panic!("Attempt to get the 5th Base Address Register, whose type is 64-bit!");
+        }
+
+        let addr = match self.bar[index].ty() {
+            BarType::Bar32Bit => (self.bar[index].as_u32() & !0xf) as u64,
+            BarType::Bar64Bit => {
+                (self.bar[index + 1].as_u32() << 32) as u64 & self.bar[index].as_u32() as u64
+            }
+        };
+
+        PhysAddr::new(addr)
     }
 }
