d5400b58c38359937fbe082a3bddf5c0f0cf01c6
toku-sa-n <tokusan441@gmail.com>
2020-09-25 10:44:00 +0900

    Mark `TypeSpecNonBridge::parse_raw` as `pub(super)`

--- kernel/src/device/pci/config/type_spec/non_bridge.rs
+++ kernel/src/device/pci/config/type_spec/non_bridge.rs
@@ -7,7 +7,7 @@ pub(super) struct TypeSpecNonBridge {
 }
 
 impl TypeSpecNonBridge {
-    fn parse_raw(raw: &RawSpace) -> Self {
+    pub(super) fn parse_raw(raw: &RawSpace) -> Self {
         let mut bar = [Bar::default(); 6];
         for i in 0..6 {
             bar[i] = Bar::new(raw.as_slice()[i + 4]);
