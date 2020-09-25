15ac97bb904c1fb1b60b588f6497566cf643672d
toku-sa-n <tokusan441@gmail.com>
2020-09-25 12:38:43 +0900

    Define `ExtendedCapabilities`

--- kernel/src/device/pci/config/extended_capability/mod.rs
+++ kernel/src/device/pci/config/extended_capability/mod.rs
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use alloc::vec::Vec;
+
+struct ExtendedCapabilities(Vec<ExtendedCapability>);
+
 pub struct ExtendedCapability {
     capability_id: u8,
     next_ptr: u8,
