c1bda5da64048d8b601c71a8c09a8db3d68ce8bf
toku-sa-n <tokusan441@gmail.com>
2020-09-25 10:44:37 +0900

    Define `TypeSpec::parse_raw`

--- kernel/src/device/pci/config/type_spec/mod.rs
+++ kernel/src/device/pci/config/type_spec/mod.rs
@@ -2,8 +2,20 @@
 
 mod non_bridge;
 
-use super::{Bar, RawSpace};
+use {
+    super::{Bar, Common, RawSpace},
+    non_bridge::TypeSpecNonBridge,
+};
 
 enum TypeSpec {
     NonBridge(non_bridge::TypeSpecNonBridge),
+}
+
+impl TypeSpec {
+    fn parse_raw(raw: &RawSpace, common: &Common) -> Self {
+        match common.header_type() {
+            0 => TypeSpec::NonBridge(TypeSpecNonBridge::parse_raw(raw)),
+            _ => todo!(),
+        }
+    }
 }
