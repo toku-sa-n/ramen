2fbd84b4dac0f093a4c6d41071a254b49a16b889
toku-sa-n <tokusan441@gmail.com>
2020-09-29 17:07:55 +0900

    Add `event_ring_segment_table`

--- kernel/src/device/pci/xhci/register/mod.rs
+++ kernel/src/device/pci/xhci/register/mod.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+pub mod event_ring_segment_table;
 pub mod hc_capability_registers;
 pub mod hc_operational_registers;
 pub mod usb_legacy_support_capability;
