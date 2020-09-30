61ef655b506683285eb208dc3eb135e380665aef
toku-sa-n <tokusan441@gmail.com>
2020-09-30 10:50:52 +0900

    Fix

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -95,5 +95,6 @@ fn run_tasks() -> ! {
     //
     // If you change the value `0xf4` and `0x10`, don't forget to change the correspond values in
     // `Makefile`!
-    qemu_exit::x86::exit::<u32, 0xf4>(0x10);
+    use qemu_exit::QEMUExit;
+    qemu_exit::X86::new(0xf4, 0x10).exit_success();
 }
