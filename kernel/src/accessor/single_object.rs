289dbc63e3f4ca2ae7e199fab8ef7a653bd16bea
toku-sa-n <tokusan441@gmail.com>
2020-10-05 11:30:33 +0900

    Clippy

--- kernel/src/accessor/single_object.rs
+++ kernel/src/accessor/single_object.rs
@@ -17,12 +17,12 @@ pub struct Accessor<'a, T: 'a> {
 
 impl<'a, T: 'a> Accessor<'a, T> {
     pub fn new(phys_base: PhysAddr, offset: usize) -> Self {
+        use x86_64::structures::paging::MapperAllSizes;
         let phys_base = phys_base + offset;
         info!("phys_base: {:?}", phys_base);
 
         let base = super::map_pages(phys_base, Size::new(size_of::<T>()));
 
-        use x86_64::structures::paging::MapperAllSizes;
         info!(
             "{:?} -> {:?}",
             base,
