5e4dc0833759d81a705944a681f9c890ae8bfff5
toku-sa-n <tokusan441@gmail.com>
2020-10-06 13:13:32 +0900

    Define `CapabilitySpec::new`

--- kernel/src/device/pci/config/extended_capability/msi.rs
+++ kernel/src/device/pci/config/extended_capability/msi.rs
@@ -7,3 +7,9 @@ pub struct CapabilitySpec<'a> {
     registers: &'a Registers,
     base: RegisterIndex,
 }
+
+impl<'a> CapabilitySpec<'a> {
+    pub fn new(registers: &'a Registers, base: RegisterIndex) -> Self {
+        Self { registers, base }
+    }
+}
