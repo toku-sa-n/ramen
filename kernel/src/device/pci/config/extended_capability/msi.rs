5d350e09d7174e45fee97b183ef6b8f35a0fc130
toku-sa-n <tokusan441@gmail.com>
2020-10-06 12:55:14 +0900

    Define `CapabilitySpec`

--- kernel/src/device/pci/config/extended_capability/msi.rs
+++ kernel/src/device/pci/config/extended_capability/msi.rs
@@ -1,1 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use super::{RegisterIndex, Registers};
+
+#[derive(Debug)]
+pub struct CapabilitySpec<'a> {
+    registers: &'a Registers,
+    base: RegisterIndex,
+}
