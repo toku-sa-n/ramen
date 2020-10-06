33938683aee3a6808745974ac75642d09b5c0d55
toku-sa-n <tokusan441@gmail.com>
2020-10-06 16:34:49 +0900

    Define `edit_message_address`

--- kernel/src/device/pci/config/extended_capability/msi.rs
+++ kernel/src/device/pci/config/extended_capability/msi.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use super::{MessageAddress, RegisterIndex, Registers};
+use super::{MessageAddress, MessageData, RegisterIndex, Registers};
 
 #[derive(Debug)]
 pub struct CapabilitySpec<'a> {
@@ -11,6 +11,15 @@ pub struct CapabilitySpec<'a> {
 impl<'a> CapabilitySpec<'a> {
     pub fn new(registers: &'a Registers, base: RegisterIndex) -> Self {
         Self { registers, base }
+    }
+
+    fn edit_message_address<T>(&self, f: T)
+    where
+        T: Fn(&mut MessageAddress),
+    {
+        let mut message_address = self.get_message_address();
+        f(&mut message_address);
+        self.set_message_address(message_address);
     }
 
     fn get_message_address(&self) -> MessageAddress {
