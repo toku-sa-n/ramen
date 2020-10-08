e73502ce1bad71f7636fbebb16a51a4a57573ebd
toku-sa-n <tokusan441@gmail.com>
2020-10-08 16:14:53 +0900

    Fix

--- kernel/src/device/pci/xhci/transfer_ring/mod.rs
+++ kernel/src/device/pci/xhci/transfer_ring/mod.rs
@@ -70,6 +70,16 @@ impl<'a> RingQueue<'a, Event> {
         }
     }
 
+    pub fn check(&self) {
+        for (i, trb) in self.queue.iter().enumerate() {
+            if trb.valid(self.cycle_bit) {
+                info!("TRB{} is valid.", i);
+            }
+        }
+
+        info!("Check finished.");
+    }
+
     fn increment_dequeue_index(&mut self) {
         self.dequeue_index += 1;
         if self.dequeue_index >= NUM_OF_TRB_IN_QUEUE {
