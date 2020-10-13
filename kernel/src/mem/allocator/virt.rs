2a2157aa93ae39f0ff010d583ec4d3ea4aafe4a3
toku-sa-n <tokusan441@gmail.com>
2020-10-13 16:34:50 +0900

    Lower the level of log

--- kernel/src/mem/allocator/virt.rs
+++ kernel/src/mem/allocator/virt.rs
@@ -12,13 +12,13 @@ use {
 };
 
 pub fn search_first_unused_page() -> Option<Page> {
-    info!("Searching free virtual address...");
+    debug!("Searching free virtual address...");
     for addr in
         (0..BYTES_AVAILABLE_RAM.as_usize()).step_by(usize::try_from(Size4KiB::SIZE).unwrap())
     {
         let virt_addr = VirtAddr::new(addr as _);
         if available(virt_addr) {
-            info!("Found: {:?}", virt_addr);
+            debug!("Found: {:?}", virt_addr);
             return Some(Page::containing_address(virt_addr));
         }
     }
