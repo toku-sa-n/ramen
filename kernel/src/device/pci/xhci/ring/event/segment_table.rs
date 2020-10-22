c62c57b2ada9ca1f6a15047983faafb838370101
toku-sa-n <tokusan441@gmail.com>
2020-10-22 16:30:16 +0900

    Define `segment_table::Entry`

--- kernel/src/device/pci/xhci/ring/event/segment_table.rs
+++ kernel/src/device/pci/xhci/ring/event/segment_table.rs
@@ -1,1 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use x86_64::PhysAddr;
+
+#[repr(C, packed)]
+struct Entry {
+    base_address: u64,
+    segment_size: u64,
+}
+impl Entry {
+    // Although the size of segment_size is u64, bits 16:63 are reserved.
+    fn set(&mut self, addr: PhysAddr, size: u16) {
+        self.base_address = addr.as_u64();
+        self.segment_size = u64::from(size);
+    }
+}
