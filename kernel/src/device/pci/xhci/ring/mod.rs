3e0b2bd8665ceae5e354b974e01f6d17bd5c1526
toku-sa-n <tokusan441@gmail.com>
2020-10-22 16:58:17 +0900

    Define `Raw`

--- kernel/src/device/pci/xhci/ring/mod.rs
+++ kernel/src/device/pci/xhci/ring/mod.rs
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use {
+    crate::{accessor::slice, mem::allocator::phys::FRAME_MANAGER},
+    os_units::Size,
+    x86_64::structures::paging::{FrameAllocator, PageSize, Size4KiB},
+};
+
 mod event;
 mod trb;
+
+struct Raw<'a>(slice::Accessor<'a, u128>);
+impl<'a> Raw<'a> {
+    fn new(num_trb: usize) -> Self {
+        assert!(num_trb as u64 <= Size4KiB::SIZE / 16);
+        let phys_frame = FRAME_MANAGER.lock().allocate_frame().unwrap();
+        let addr = phys_frame.start_address();
+        Self(slice::Accessor::new(addr, Size::new(0), num_trb))
+    }
+}
