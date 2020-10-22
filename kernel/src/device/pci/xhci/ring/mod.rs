1af241bb1a4757a062158071742e705772c8ed0d
toku-sa-n <tokusan441@gmail.com>
2020-10-22 17:11:55 +0900

    Define indexing

--- kernel/src/device/pci/xhci/ring/mod.rs
+++ kernel/src/device/pci/xhci/ring/mod.rs
@@ -2,6 +2,7 @@
 
 use {
     crate::{accessor::slice, mem::allocator::phys::FRAME_MANAGER},
+    core::ops::{Index, IndexMut},
     os_units::Size,
     x86_64::structures::paging::{FrameAllocator, PageSize, Size4KiB},
 };
@@ -16,5 +17,16 @@ impl<'a> Raw<'a> {
         let phys_frame = FRAME_MANAGER.lock().allocate_frame().unwrap();
         let addr = phys_frame.start_address();
         Self(slice::Accessor::new(addr, Size::new(0), num_trb))
+    }
+}
+impl<'a> Index<usize> for Raw<'a> {
+    type Output = u128;
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+impl<'a> IndexMut<usize> for Raw<'a> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
     }
 }
