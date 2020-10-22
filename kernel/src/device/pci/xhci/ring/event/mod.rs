35086f93fdb10b522330bc4f4bc2ad707df0066f
toku-sa-n <tokusan441@gmail.com>
2020-10-22 17:15:50 +0900

    Define `EventRing::new`

--- kernel/src/device/pci/xhci/ring/event/mod.rs
+++ kernel/src/device/pci/xhci/ring/event/mod.rs
@@ -7,3 +7,10 @@ mod segment_table;
 struct EventRing<'a> {
     raw: Raw<'a>,
 }
+impl<'a> EventRing<'a> {
+    fn new(num_trb: usize) -> Self {
+        Self {
+            raw: Raw::new(num_trb),
+        }
+    }
+}
