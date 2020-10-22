31a9ddc730e7a267169991aa30d9bff1714d0e2b
toku-sa-n <tokusan441@gmail.com>
2020-10-22 15:49:10 +0900

    Define `Noop`

--- kernel/src/device/pci/xhci/transfer_ring/block.rs
+++ kernel/src/device/pci/xhci/transfer_ring/block.rs
@@ -1,1 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use {super::CycleBit, bitfield::bitfield};
+
+bitfield! {
+    #[repr(transparent)]
+    pub struct Noop(u128);
+
+    _, set_cycle_bit: 96;
+    _, set_trb_type: 96+15, 96+10;
+}
+impl Noop {
+    fn new(cycle_bit: CycleBit) -> Self {
+        let mut noop = Noop(0);
+        noop.set_cycle_bit(cycle_bit.into());
+        noop.set_trb_type(8);
+
+        noop
+    }
+}
