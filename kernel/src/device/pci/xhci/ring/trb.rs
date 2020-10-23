24b6aa0a42be86e84ab93f4e8c9e86705628c73d
toku-sa-n <tokusan441@gmail.com>
2020-10-23 12:26:11 +0900

    Convert from u128 to Trb

--- kernel/src/device/pci/xhci/ring/trb.rs
+++ kernel/src/device/pci/xhci/ring/trb.rs
@@ -1,13 +1,40 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use {super::CycleBit, bitfield::bitfield};
+use {super::CycleBit, bitfield::bitfield, core::convert::TryFrom};
 
 enum Ty {
     Noop = 8,
 }
+impl TryFrom<u128> for Ty {
+    type Error = Error;
 
+    fn try_from(value: u128) -> Result<Self, Self::Error> {
+        let error_num = (value >> 106) & 0x3f;
+
+        match error_num {
+            x if x == Self::Noop as _ => Ok(Self::Noop),
+            _ => Err(Error::InvalidId),
+        }
+    }
+}
 enum Trb {
     Noop(Noop),
+}
+impl TryFrom<u128> for Trb {
+    type Error = Error;
+
+    fn try_from(value: u128) -> Result<Self, Self::Error> {
+        match Ty::try_from(value) {
+            Ok(ty) => match ty {
+                Ty::Noop => Ok(Self::Noop(Noop::from(value))),
+            },
+            Err(_) => Err(Error::InvalidId),
+        }
+    }
+}
+
+enum Error {
+    InvalidId,
 }
 
 bitfield! {
@@ -24,5 +51,10 @@ impl Noop {
         noop.set_trb_type(Ty::Noop as _);
 
         noop
+    }
+}
+impl From<u128> for Noop {
+    fn from(value: u128) -> Self {
+        Self(value)
     }
 }
