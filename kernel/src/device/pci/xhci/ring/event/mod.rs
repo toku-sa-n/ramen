c89a95cd4ae42e74419e6e3e1948981c8e7d8a2c
toku-sa-n <tokusan441@gmail.com>
2020-10-23 11:08:41 +0900

    Add `dequeue_ptr` as a member

--- kernel/src/device/pci/xhci/ring/event/mod.rs
+++ kernel/src/device/pci/xhci/ring/event/mod.rs
@@ -7,12 +7,14 @@ mod segment_table;
 struct EventRing<'a> {
     raw: Raw<'a>,
     current_cycle_bit: CycleBit,
+    dequeue_ptr: usize,
 }
 impl<'a> EventRing<'a> {
     fn new(num_trb: usize) -> Self {
         Self {
             raw: Raw::new(num_trb),
             current_cycle_bit: CycleBit::new(true),
+            dequeue_ptr: 0,
         }
     }
 }
