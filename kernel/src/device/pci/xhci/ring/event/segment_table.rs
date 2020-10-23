c1488e646c66a3794a4044c93f418752a6461a8d
toku-sa-n <tokusan441@gmail.com>
2020-10-23 18:14:19 +0900

    Define `SegmentTable::phys_addr`

--- kernel/src/device/pci/xhci/ring/event/segment_table.rs
+++ kernel/src/device/pci/xhci/ring/event/segment_table.rs
@@ -6,20 +6,22 @@ use {
     x86_64::PhysAddr,
 };
 
-pub struct SegmentTable<'a> {
-    addr: PhysAddr,
-    table: slice::Accessor<'a, Entry>,
+pub struct SegmentTable<'a>(slice::Accessor<'a, Entry>);
+impl<'a> SegmentTable<'a> {
+    fn phys_addr(&self) -> PhysAddr {
+        self.0.phys_base()
+    }
 }
 impl<'a> Index<usize> for SegmentTable<'a> {
     type Output = Entry;
 
     fn index(&self, index: usize) -> &Self::Output {
-        &self.table[index]
+        &self.0[index]
     }
 }
 impl<'a> IndexMut<usize> for SegmentTable<'a> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.table[index]
+        &mut self.0[index]
     }
 }
 
@@ -30,7 +32,7 @@ struct Entry {
 }
 impl Entry {
     // Although the size of segment_size is u64, bits 16:63 are reserved.
-    fn set(&mut self, addr: PhysAddr, size: u16) {
+    pub fn set(&mut self, addr: PhysAddr, size: u16) {
         self.base_address = addr.as_u64();
         self.segment_size = u64::from(size);
     }
