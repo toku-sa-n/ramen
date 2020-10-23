58b88a450e552cf783db099307fe96773419592f
toku-sa-n <tokusan441@gmail.com>
2020-10-23 17:26:36 +0900

    Define indexing

--- kernel/src/device/pci/xhci/ring/event/segment_table.rs
+++ kernel/src/device/pci/xhci/ring/event/segment_table.rs
@@ -1,10 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use {crate::mem::accessor::slice, x86_64::PhysAddr};
+use {
+    crate::mem::accessor::slice,
+    core::ops::{Index, IndexMut},
+    x86_64::PhysAddr,
+};
 
 pub struct SegmentTable<'a> {
     addr: PhysAddr,
     table: slice::Accessor<'a, Entry>,
+}
+impl<'a> Index<usize> for SegmentTable<'a> {
+    type Output = Entry;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.table[index]
+    }
+}
+impl<'a> IndexMut<usize> for SegmentTable<'a> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.table[index]
+    }
 }
 
 #[repr(C, packed)]
