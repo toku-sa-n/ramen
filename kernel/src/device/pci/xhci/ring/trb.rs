e337712ca19848583c9e7dcd698449ccfbbc978a
toku-sa-n <tokusan441@gmail.com>
2020-10-27 17:28:28 +0900

    Remove unnecessary parentheses

--- kernel/src/device/pci/xhci/ring/trb.rs
+++ kernel/src/device/pci/xhci/ring/trb.rs
@@ -55,7 +55,7 @@ bitfield! {
     pub struct Noop(u128);
 
     _, set_cycle_bit: 96;
-    trb_type, set_trb_type: (96+15), (96+10);
+    trb_type, set_trb_type: 96+15, 96+10;
 }
 impl Noop {
     fn new(cycle_bit: CycleBit) -> Self {
