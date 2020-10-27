fd574eb3c1df31739fcfa9b0ca44e89c518f93d9
toku-sa-n <tokusan441@gmail.com>
2020-10-27 10:50:08 +0900

    Remove `len`
    Use `len` method of `Raw`

--- kernel/src/device/pci/xhci/ring/command.rs
+++ kernel/src/device/pci/xhci/ring/command.rs
@@ -5,14 +5,12 @@ use {super::Raw, x86_64::PhysAddr};
 pub struct Ring {
     raw: Raw,
     enqueue_ptr: usize,
-    len: usize,
 }
 impl Ring {
     pub fn new(len: usize) -> Self {
         Self {
             raw: Raw::new(len),
             enqueue_ptr: 0,
-            len,
         }
     }
 
