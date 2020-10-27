bc3ab7ad9a52f7ccddf689491081a9218107d695
toku-sa-n <tokusan441@gmail.com>
2020-10-27 12:37:49 +0900

    Define doorbell array

--- kernel/src/device/pci/xhci/register/doorbell.rs
+++ kernel/src/device/pci/xhci/register/doorbell.rs
@@ -1,1 +1,27 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use {
+    super::hc_capability_registers::DoorbellOffset, crate::mem::accessor::Accessor,
+    core::convert::TryInto, os_units::Bytes, x86_64::PhysAddr,
+};
+
+const NUM_OF_REGISTERS: usize = 256;
+
+struct Array(Accessor<[Register]>);
+impl Array {
+    fn new(mmio_base: PhysAddr, db_off: DoorbellOffset) -> Self {
+        Self(Accessor::new_slice(
+            mmio_base,
+            Bytes::new(db_off.get().try_into().unwrap()),
+            NUM_OF_REGISTERS,
+        ))
+    }
+}
+
+#[repr(transparent)]
+struct Register(u32);
+impl Register {
+    fn write_for_hc(&mut self) {
+        self.0 = 0;
+    }
+}
