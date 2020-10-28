fd1262958aa1e63c1b14cd30a85aabcfe3b18328
toku-sa-n <tokusan441@gmail.com>
2020-10-28 15:33:36 +0900

    Remove lines about the old implementation of memory manager

--- bootx64/src/mem/free_page.rs
+++ bootx64/src/mem/free_page.rs
@@ -1,18 +1,1 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-
-use uefi::{
-    table::{
-        boot,
-        boot::{AllocateType, MemoryType},
-    },
-    ResultExt,
-};
-use x86_64::PhysAddr;
-
-pub fn allocate(boot_services: &boot::BootServices) -> PhysAddr {
-    PhysAddr::new(
-        boot_services
-            .allocate_pages(AllocateType::AnyPages, MemoryType::LOADER_DATA, 1)
-            .expect_success("Failed to allocate memory for free memory"),
-    )
-}
