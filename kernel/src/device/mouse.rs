ab5711e50f342844b8a159955441212515491ede
toku-sa-n <tokusan441@gmail.com>
2020-10-31 22:30:43 +0900

    Define submethods

--- kernel/src/device/mouse.rs
+++ kernel/src/device/mouse.rs
@@ -143,13 +143,7 @@ impl Buf {
 
     fn put(&mut self, packet: u8) {
         match self.phase {
-            DevicePhase::Init => {
-                let is_correct_startup = packet == 0xfa;
-                if is_correct_startup {
-                    self.phase = DevicePhase::NoData
-                }
-            }
-
+            DevicePhase::Init => self.check_ack_packet(packet),
             DevicePhase::NoData => {
                 if Self::is_correct_first_byte_from_device(packet) {
                     self.packets[0] = packet;
@@ -166,6 +160,16 @@ impl Buf {
             }
             DevicePhase::ThreeData => {}
         }
+    }
+
+    fn check_ack_packet(&mut self, packet: u8) {
+        if Self::is_ack_packet(packet) {
+            self.phase = DevicePhase::NoData
+        }
+    }
+
+    fn is_ack_packet(packet: u8) -> bool {
+        packet == 0xfa
     }
 
     // To sync phase, and data sent from mouse device
