c2257928dd8997f6e082611276807b7cfca242c3
toku-sa-n <tokusan441@gmail.com>
2020-11-01 18:49:30 +0900

    Define `Registers`

--- kernel/src/device/pci/xhci/register/mod.rs
+++ kernel/src/device/pci/xhci/register/mod.rs
@@ -5,3 +5,17 @@ pub mod hc_capability_registers;
 pub mod hc_operational_registers;
 pub mod runtime_base_registers;
 pub mod usb_legacy_support_capability;
+
+use {
+    hc_capability_registers::HCCapabilityRegisters,
+    hc_operational_registers::HCOperationalRegisters, runtime_base_registers::RuntimeBaseRegisters,
+    usb_legacy_support_capability::UsbLegacySupportCapability,
+};
+
+pub struct Registers {
+    usb_legacy_support_capability: Option<UsbLegacySupportCapability>,
+    hc_capability_registers: HCCapabilityRegisters,
+    hc_operational_registers: HCOperationalRegisters,
+    runtime_base_registers: RuntimeBaseRegisters,
+    doorbell_array: doorbell::Array,
+}
