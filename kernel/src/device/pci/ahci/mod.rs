5766dac520853b8fe4ad1b4d2dc591c6e7e76977
toku-sa-n <tokusan441@gmail.com>
2020-11-14 21:57:57 +0900

    chore: fetch ABAR

--- kernel/src/device/pci/ahci/mod.rs
+++ kernel/src/device/pci/ahci/mod.rs
@@ -1,9 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use {super::config::bar, x86_64::PhysAddr};
+
 pub async fn task() {
-    for device in super::iter_devices() {
-        if device.is_ahci() {
-            info!("SATA");
+    let abar = match AchiBaseAddr::new() {
+        Some(abar) => abar,
+        None => return,
+    };
+}
+
+struct AchiBaseAddr(PhysAddr);
+impl AchiBaseAddr {
+    fn new() -> Option<Self> {
+        for device in super::iter_devices() {
+            if device.is_ahci() {
+                let addr = device.base_address(bar::Index::new(5));
+                return Some(Self(addr));
+            }
         }
+
+        None
     }
 }
