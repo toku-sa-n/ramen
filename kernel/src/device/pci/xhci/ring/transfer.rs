cf6e13eef98fc6d7bb762fdc66690f8b317669e7
toku-sa-n <tokusan441@gmail.com>
2020-11-20 14:49:46 +0900

    chore: implement `TransferRing::enqueue`

--- kernel/src/device/pci/xhci/ring/transfer.rs
+++ kernel/src/device/pci/xhci/ring/transfer.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use super::{super::Registers, raw, CycleBit};
+use super::{super::Registers, raw, trb::Trb, CycleBit};
 use alloc::rc::Rc;
 use core::cell::RefCell;
 use x86_64::PhysAddr;
@@ -26,4 +26,43 @@ impl Ring {
     pub fn phys_addr(&self) -> PhysAddr {
         self.raw.phys_addr()
     }
+
+    fn enqueue(&mut self, trb: Trb) -> Result<PhysAddr, Error> {
+        if self.full() {
+            return Err(Error::QueueIsFull);
+        }
+
+        self.raw[self.enqueue_ptr] = trb.into();
+
+        let addr_to_trb = self.addr_to_enqueue_ptr();
+        self.enqueue_ptr += 1;
+        if self.enqueue_ptr < self.len() {
+            return Ok(addr_to_trb);
+        }
+
+        self.raw[self.enqueue_ptr] = Trb::new_link(self.phys_addr(), self.cycle_bit).into();
+
+        self.enqueue_ptr = 0;
+        self.cycle_bit.toggle();
+
+        Ok(addr_to_trb)
+    }
+
+    fn full(&self) -> bool {
+        let raw = self.raw[self.enqueue_ptr];
+        raw.cycle_bit() == self.cycle_bit
+    }
+
+    fn addr_to_enqueue_ptr(&self) -> PhysAddr {
+        self.phys_addr() + Trb::SIZE.as_usize() * self.enqueue_ptr
+    }
+
+    fn len(&self) -> usize {
+        self.raw.len()
+    }
+}
+
+#[derive(Debug)]
+enum Error {
+    QueueIsFull,
 }
