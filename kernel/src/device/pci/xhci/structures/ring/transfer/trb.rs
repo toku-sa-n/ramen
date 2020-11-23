f15cdbfc52b749f449b56f3d030accdf576f42d2
toku-sa-n <tokusan441@gmail.com>
2020-11-23 14:54:25 +0900

    refactor: define `Control`

--- kernel/src/device/pci/xhci/structures/ring/transfer/trb.rs
+++ kernel/src/device/pci/xhci/structures/ring/transfer/trb.rs
@@ -8,9 +8,7 @@ use os_units::Bytes;
 use x86_64::PhysAddr;
 
 pub enum Trb {
-    SetupStage(SetupStage),
-    DataStage(DataStage),
-    StatusStage(StatusStage),
+    Control(Control),
 }
 impl Trb {
     pub const SIZE: Bytes = Bytes::new(16);
@@ -30,6 +28,12 @@ impl From<Trb> for raw::Trb {
     fn from(t: Trb) -> Self {
         unimplemented!()
     }
+}
+
+pub enum Control {
+    Setup(SetupStage),
+    Data(DataStage),
+    Status(StatusStage),
 }
 
 add_trb!(SetupStage);
