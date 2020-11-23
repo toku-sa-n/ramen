61477dbcb1c1ea19ac023fd2f53b6a1ab76f7284
toku-sa-n <tokusan441@gmail.com>
2020-11-23 21:41:14 +0900

    chore: define `command::Raw`

--- kernel/src/device/pci/xhci/structures/ring/command/mod.rs
+++ kernel/src/device/pci/xhci/structures/ring/command/mod.rs
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use super::{super::registers::Registers, raw, CycleBit};
+use crate::mem::allocator::page_box::PageBox;
 use alloc::rc::Rc;
+use bit_field::BitField;
 use core::cell::RefCell;
 use trb::Trb;
 use x86_64::PhysAddr;
@@ -121,6 +123,107 @@ impl<'a> Ring {
     fn move_enqueue_ptr_to_the_beginning(&mut self) {
         self.enqueue_ptr = 0;
         self.cycle_bit.toggle();
+    }
+}
+
+struct Raw {
+    raw: PageBox<[[u32; 4]]>,
+    enq_p: usize,
+    c: CycleBit,
+}
+impl Raw {
+    fn new() -> Self {
+        Self {
+            raw: PageBox::new_slice([0; 4], SIZE_OF_RING),
+            enq_p: 0,
+            c: CycleBit::new(true),
+        }
+    }
+
+    fn try_enqueue(&mut self, trb: Trb) -> Result<PhysAddr, Error> {
+        if self.enqueueable() {
+            Ok(self.enqueue(trb))
+        } else {
+            Err(Error::QueueIsFull)
+        }
+    }
+
+    fn enqueueable(&self) -> bool {
+        !self.full() && self.has_space_for_link_trb()
+    }
+
+    fn full(&self) -> bool {
+        self.writable(self.enq_p)
+    }
+
+    fn has_space_for_link_trb(&self) -> bool {
+        if self.wrapping_back_happens() {
+            self.writable(self.next_enq_p())
+        } else {
+            true
+        }
+    }
+
+    fn wrapping_back_happens(&self) -> bool {
+        self.next_enq_p() == 0
+    }
+
+    fn writable(&self, i: usize) -> bool {
+        self.c_bit(i) != self.c
+    }
+
+    fn c_bit(&self, i: usize) -> CycleBit {
+        let t = self.raw[i];
+        CycleBit::new(t[3].get_bit(0))
+    }
+
+    fn next_enq_p(&self) -> usize {
+        (self.enq_p + 1) % SIZE_OF_RING
+    }
+
+    fn enqueue(&mut self, trb: Trb) -> PhysAddr {
+        self.write_trb(trb);
+        let trb_a = self.enq_addr();
+        self.increment();
+        trb_a
+    }
+
+    fn write_trb(&mut self, trb: Trb) {
+        self.raw[self.enq_p] = trb.into();
+    }
+
+    fn increment(&mut self) {
+        self.enq_p += 1;
+        if !self.enq_p_within_ring() {
+            self.enq_link();
+            self.move_enq_p_to_the_beginning();
+        }
+    }
+
+    fn enq_p_within_ring(&self) -> bool {
+        self.enq_p < self.len() - 1
+    }
+
+    fn enq_link(&mut self) {
+        // Don't call `enqueue`. It will return an `Err` value as there is no space for link TRB.
+        self.raw[self.enq_p] = Trb::new_link(self.head_addr(), self.c).into();
+    }
+
+    fn move_enq_p_to_the_beginning(&mut self) {
+        self.enq_p = 0;
+        self.c.toggle();
+    }
+
+    fn enq_addr(&self) -> PhysAddr {
+        self.head_addr() + Trb::SIZE.as_usize() * self.enq_p
+    }
+
+    fn head_addr(&self) -> PhysAddr {
+        self.raw.phys_addr()
+    }
+
+    fn len(&self) -> usize {
+        self.raw.len()
     }
 }
 
