565cc7c87da20baaf8e3db2632062d76e485fb92
toku-sa-n <tokusan441@gmail.com>
2020-11-24 10:28:43 +0900

    chore: define `event::Raw`

--- kernel/src/device/pci/xhci/structures/ring/event/mod.rs
+++ kernel/src/device/pci/xhci/structures/ring/event/mod.rs
@@ -3,9 +3,11 @@
 use super::{super::registers::Registers, raw, CycleBit};
 use crate::{
     device::pci::xhci::exchanger::receiver::Receiver,
+    mem::allocator::page_box::PageBox,
     multitask::task::{self, Task},
 };
 use alloc::{rc::Rc, vec::Vec};
+use bit_field::BitField;
 use core::{
     cell::RefCell,
     convert::{TryFrom, TryInto},
@@ -39,6 +41,9 @@ pub async fn task(mut ring: Ring, command_completion_receiver: Rc<RefCell<Receiv
     }
 }
 
+#[allow(clippy::cast_possible_truncation)]
+const MAX_NUM_OF_TRB_IN_QUEUE: u16 = Size4KiB::SIZE as u16 / Trb::SIZE.as_usize() as u16;
+
 pub struct Ring {
     arrays: Vec<raw::Ring>,
     segment_table: SegmentTable,
@@ -49,9 +54,6 @@ pub struct Ring {
     registers: Rc<RefCell<Registers>>,
 }
 impl<'a> Ring {
-    #[allow(clippy::cast_possible_truncation)]
-    const MAX_NUM_OF_TRB_IN_QUEUE: u16 = Size4KiB::SIZE as u16 / Trb::SIZE.as_usize() as u16;
-
     pub fn new(
         registers: Rc<RefCell<Registers>>,
         task_collection: Rc<RefCell<task::Collection>>,
@@ -94,7 +96,7 @@ impl<'a> Ring {
 
     fn register_addresses_of_arrays_to_segment_table(&mut self) {
         for i in 0..self.segment_table.len() {
-            self.segment_table[i].set(self.arrays[0].phys_addr(), Self::MAX_NUM_OF_TRB_IN_QUEUE);
+            self.segment_table[i].set(self.arrays[0].phys_addr(), MAX_NUM_OF_TRB_IN_QUEUE);
         }
     }
 
@@ -109,7 +111,7 @@ impl<'a> Ring {
     fn new_arrays(max_num_of_erst: u16) -> Vec<raw::Ring> {
         let mut arrays = Vec::new();
         for _ in 0_u16..max_num_of_erst {
-            arrays.push(raw::Ring::new(Self::MAX_NUM_OF_TRB_IN_QUEUE.into()));
+            arrays.push(raw::Ring::new(MAX_NUM_OF_TRB_IN_QUEUE.into()));
         }
 
         arrays
@@ -133,7 +135,7 @@ impl<'a> Ring {
 
     fn increment(&mut self) {
         self.dequeue_ptr_trb += 1;
-        if self.dequeue_ptr_trb >= Self::MAX_NUM_OF_TRB_IN_QUEUE.into() {
+        if self.dequeue_ptr_trb >= MAX_NUM_OF_TRB_IN_QUEUE.into() {
             self.dequeue_ptr_trb = 0;
             self.dequeue_ptr_segment += 1;
 
@@ -179,4 +181,98 @@ impl<'a> Stream for Ring {
             },
         )
     }
+}
+
+struct Raw {
+    rings: Vec<PageBox<[[u32; 4]]>>,
+    c: CycleBit,
+    deq_p_seg: usize,
+    deq_p_trb: usize,
+    r: Rc<RefCell<Registers>>,
+}
+impl Raw {
+    fn new(r: Rc<RefCell<Registers>>) -> Self {
+        Self {
+            rings: Self::new_rings(&r),
+            c: CycleBit::new(true),
+            deq_p_seg: 0,
+            deq_p_trb: 0,
+            r,
+        }
+    }
+
+    fn new_rings(r: &Rc<RefCell<Registers>>) -> Vec<PageBox<[[u32; 4]]>> {
+        let mut v = Vec::new();
+        for _ in 0..Self::max_num_of_erst(r) {
+            v.push(PageBox::new_slice([0; 4], MAX_NUM_OF_TRB_IN_QUEUE.into()));
+        }
+
+        v
+    }
+
+    fn max_num_of_erst(r: &Rc<RefCell<Registers>>) -> u16 {
+        let p2 = r.borrow().capability.hcs_params_2.read();
+        p2.powered_erst_max()
+    }
+
+    fn try_dequeue(&mut self) -> Option<Trb> {
+        if self.empty() {
+            None
+        } else {
+            self.dequeue()
+        }
+    }
+
+    fn empty(&self) -> bool {
+        self.c_bit_of_next_trb() != self.c
+    }
+
+    fn c_bit_of_next_trb(&self) -> CycleBit {
+        let t = self.rings[self.deq_p_seg][self.deq_p_trb];
+        CycleBit::new(t[3].get_bit(0))
+    }
+
+    fn dequeue(&mut self) -> Option<Trb> {
+        let t = self.get_next_trb().ok();
+        self.increment();
+        t
+    }
+
+    fn get_next_trb(&self) -> Result<Trb, Error> {
+        let t = self.rings[self.deq_p_seg][self.deq_p_trb];
+        t.try_into().or_else(|_| {
+            warn!("Unrecognized TRB ID {}", t[3].get_bits(10..=15));
+            Err(Error::UnrecognizedTrb)
+        })
+    }
+
+    fn increment(&mut self) {
+        self.deq_p_trb += 1;
+        if self.deq_p_trb >= MAX_NUM_OF_TRB_IN_QUEUE.into() {
+            self.deq_p_trb = 0;
+            self.deq_p_seg += 1;
+
+            if self.deq_p_seg >= self.num_of_erst() {
+                self.deq_p_seg = 0;
+                self.c.toggle();
+            }
+        }
+    }
+
+    fn num_of_erst(&self) -> usize {
+        self.rings.len()
+    }
+
+    fn update_deq_p_with_xhci(&self) {
+        let p = &mut self.r.borrow_mut().runtime.erd_p;
+        p.update(|p| p.set(self.next_trb_addr()));
+    }
+
+    fn next_trb_addr(&self) -> PhysAddr {
+        self.rings[self.deq_p_seg].phys_addr() + Trb::SIZE.as_usize() * self.deq_p_trb
+    }
+}
+
+enum Error {
+    UnrecognizedTrb,
 }
