b36a9119337cbf83fea32020210dd7bf35fe70e1
toku-sa-n <tokusan441@gmail.com>
2020-11-26 15:54:34 +0900

    chore: define fields of the Device descriptor

--- kernel/src/device/pci/xhci/structures/descriptor.rs
+++ kernel/src/device/pci/xhci/structures/descriptor.rs
@@ -2,4 +2,19 @@
 
 // Temporary implementation.
 #[derive(Default, Debug)]
-pub struct Device([u8; 18]);
+pub struct Device {
+    len: u8,
+    descriptor_type: u8,
+    cd_usb: u16,
+    class: u8,
+    subclass: u8,
+    protocol: u8,
+    max_packet_size0: u8,
+    vendor: u16,
+    product_id: u16,
+    device: u16,
+    manufacture: u8,
+    product: u8,
+    serial_number: u8,
+    num_configurations: u8,
+}
