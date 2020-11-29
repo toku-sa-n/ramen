f268296ac8237fabe398b8359cdaa01f411f98a6
toku-sa-n <tokusan441@gmail.com>
2020-11-29 17:53:51 +0900

    chore: define `Endpoint`

--- kernel/src/device/pci/xhci/port/slot/endpoint.rs
+++ kernel/src/device/pci/xhci/port/slot/endpoint.rs
@@ -1,1 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use crate::device::pci::xhci::structures::{context::Context, descriptor};
+use alloc::rc::Rc;
+use core::cell::RefCell;
+
+pub struct Endpoint {
+    desc: descriptor::Endpoint,
+    cx: Rc<RefCell<Context>>,
+}
+impl Endpoint {
+    pub fn new(desc: descriptor::Endpoint, cx: Rc<RefCell<Context>>) -> Self {
+        Self { desc, cx }
+    }
+}
