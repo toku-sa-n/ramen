7d3612da8a78175f8ff37fcab337a150e7d5a6db
toku-sa-n <tokusan441@gmail.com>
2020-11-29 23:49:04 +0900

    refactor: remove unused lines

--- kernel/src/device/pci/xhci/port/slot/mod.rs
+++ kernel/src/device/pci/xhci/port/slot/mod.rs
@@ -4,21 +4,14 @@ use super::{super::structures::descriptor::Descriptor, Port};
 use crate::{
     device::pci::xhci::{
         exchanger::{command, receiver::Receiver, transfer},
-        structures::{
-            context::{self, Context},
-            dcbaa::DeviceContextBaseAddressArray,
-            descriptor,
-            ring::CycleBit,
-        },
+        structures::{context::Context, dcbaa::DeviceContextBaseAddressArray, descriptor},
     },
     mem::allocator::page_box::PageBox,
 };
 use alloc::{rc::Rc, vec::Vec};
-use bit_field::BitField;
 use core::cell::RefCell;
 use endpoint::Endpoint;
 use futures_intrusive::sync::LocalMutex;
-use num_traits::FromPrimitive;
 use transfer::DoorbellWriter;
 
 pub mod endpoint;
@@ -123,68 +116,5 @@ impl RawDescriptorParser {
         let v = self.raw[self.current..(self.current + len)].to_vec();
         self.current += len;
         v
-    }
-}
-
-struct EndpointContextInitializer<'a> {
-    ep: &'a descriptor::Endpoint,
-    context: &'a mut Context,
-}
-impl<'a> EndpointContextInitializer<'a> {
-    fn new(ep: &'a descriptor::Endpoint, context: &'a mut Context) -> Self {
-        Self { ep, context }
-    }
-
-    fn init(&mut self) {
-        self.set_aflag();
-        self.init_ep_context();
-    }
-
-    fn set_aflag(&mut self) {
-        let dci: usize = self.calculate_dci().into();
-
-        self.context.input.control_mut().clear_aflag(1); // See xHCI dev manual 4.6.6.
-        self.context.input.control_mut().set_aflag(dci);
-    }
-
-    fn calculate_dci(&self) -> u8 {
-        let a = self.ep.endpoint_address;
-        2 * a.get_bits(0..=3) + a.get_bit(7) as u8
-    }
-
-    fn init_ep_context(&mut self) {
-        let ep_ty = self.ep_ty();
-        let max_packet_size = self.ep.max_packet_size;
-        let interval = self.ep.interval;
-
-        let c = self.ep_context();
-        c.set_endpoint_type(ep_ty);
-        c.set_max_packet_size(max_packet_size);
-        c.set_max_burst_size(0);
-        c.set_dequeue_cycle_state(CycleBit::new(true));
-        c.set_max_primary_streams(0);
-        c.set_mult(0);
-        c.set_error_count(3);
-        c.set_interval(interval);
-    }
-
-    fn ep_context(&mut self) -> &mut context::Endpoint {
-        let ep_idx: usize = self.ep.endpoint_address.get_bits(0..=3).into();
-        let out_input = self.ep.endpoint_address.get_bit(7);
-        let context_inout = &mut self.context.output_device.ep_inout[ep_idx];
-        if out_input {
-            &mut context_inout.input
-        } else {
-            &mut context_inout.out
-        }
-    }
-
-    fn ep_ty(&self) -> context::EndpointType {
-        context::EndpointType::from_u8(if self.ep.attributes == 0 {
-            0
-        } else {
-            self.ep.attributes + if self.ep.endpoint_address == 0 { 0 } else { 4 }
-        })
-        .unwrap()
     }
 }
