c4329aa2e7692b87bf65b8263dc7d7bf89bbd793
toku-sa-n <tokusan441@gmail.com>
2020-12-12 21:54:08 +0900

    chore: define `syscall::register`

--- kernel/src/syscall.rs
+++ kernel/src/syscall.rs
@@ -1,5 +1,22 @@
+use core::convert::TryInto;
+
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 pub fn syscall() {
     info!("This is `syscall` function.");
+}
+
+pub fn register() {
+    let addr = syscall as u64;
+    let l: u32 = (addr & 0xffff_ffff).try_into().unwrap();
+    let u: u32 = (addr >> 32).try_into().unwrap();
+
+    unsafe {
+        asm!("
+        mov edx, {:e}
+        mov eax, {:e}
+        mov ecx, 0xc0000082
+        wrmsr
+        ",in(reg) u,in(reg) l);
+    }
 }
