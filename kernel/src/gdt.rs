ab5be82d580c829d8aa9a70d6575c710e32ef9c9
toku-sa-n <tokusan441@gmail.com>
2020-12-13 14:56:13 +0900

    chore: define `enter_usermode` function

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -13,9 +13,9 @@ use conquer_once::spin::Lazy;
 pub static GDT: Lazy<Gdt> = Lazy::new(|| {
     let mut gdt = GlobalDescriptorTable::new();
     let kernel_code = gdt.add_entry(Descriptor::kernel_code_segment());
+    let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
     let user_code = gdt.add_entry(Descriptor::user_code_segment());
     let user_data = gdt.add_entry(Descriptor::user_data_segment());
-    let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
 
     Gdt {
         table: gdt,
@@ -46,5 +46,27 @@ pub fn init() {
         segmentation::load_gs(null_seg);
         segmentation::load_ss(null_seg);
         tables::load_tss(GDT.tss_selector);
+    }
+}
+
+pub fn enter_usermode() {
+    unsafe {
+        segmentation::load_ds(GDT.user_data);
+        segmentation::load_es(GDT.user_data);
+        segmentation::load_fs(GDT.user_data);
+        segmentation::load_gs(GDT.user_data);
+
+        asm!("
+                push {ss}
+                mov rax, rsp
+                push rax
+                pushfq
+                ", ss=in(reg) u64::from(GDT.user_data.0));
+        asm!("
+                push {sel}
+                lea {tmp}, [1f + rip]
+                push {tmp}
+                iretq
+                1:", sel=in(reg) u64::from(GDT.user_code.0),tmp=lateout(reg) _,);
     }
 }
