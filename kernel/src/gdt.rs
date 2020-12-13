2ea272e9ee1abfb06f06c25109d8bdf41e1db0dd
toku-sa-n <tokusan441@gmail.com>
2020-12-13 10:33:39 +0900

    chore: add `user_code` and `user_data` as members of `Gdt`
    To enter the user mode.

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -12,28 +12,32 @@ use conquer_once::spin::Lazy;
 
 pub static GDT: Lazy<Gdt> = Lazy::new(|| {
     let mut gdt = GlobalDescriptorTable::new();
-    let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
-    gdt.add_entry(Descriptor::user_code_segment());
-    gdt.add_entry(Descriptor::user_data_segment());
+    let kernel_code = gdt.add_entry(Descriptor::kernel_code_segment());
+    let user_code = gdt.add_entry(Descriptor::user_code_segment());
+    let user_data = gdt.add_entry(Descriptor::user_data_segment());
     let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
 
     Gdt {
         table: gdt,
-        code_selector,
+        kernel_code,
         tss_selector,
+        user_code,
+        user_data,
     }
 });
 
 pub struct Gdt {
     table: GlobalDescriptorTable,
-    code_selector: SegmentSelector,
+    kernel_code: SegmentSelector,
     tss_selector: SegmentSelector,
+    user_code: SegmentSelector,
+    user_data: SegmentSelector,
 }
 
 pub fn init() {
     GDT.table.load();
     unsafe {
-        segmentation::set_cs(GDT.code_selector);
+        segmentation::set_cs(GDT.kernel_code);
 
         let null_seg = SegmentSelector::new(0, PrivilegeLevel::Ring0);
         segmentation::load_ds(null_seg);
