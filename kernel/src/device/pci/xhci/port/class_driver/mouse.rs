cbb54e26d45d849cad0fa6c6750f457cd3453c8d
toku-sa-n <tokusan441@gmail.com>
2020-12-17 11:04:55 +0900

    feat: read packets from USB mouse

--- kernel/src/device/pci/xhci/port/class_driver/mouse.rs
+++ kernel/src/device/pci/xhci/port/class_driver/mouse.rs
@@ -1,1 +1,42 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use crate::{
+    device::pci::xhci::{port::endpoint, structures::context::EndpointType},
+    mem::allocator::page_box::PageBox,
+};
+
+pub async fn task(mut mouse: Mouse) {
+    loop {
+        mouse.get_packet().await;
+        mouse.print_buf();
+    }
+}
+
+pub struct Mouse {
+    ep: endpoint::Collection,
+    buf: PageBox<[u8; 4]>,
+}
+impl Mouse {
+    pub fn new(ep: endpoint::Collection) -> Self {
+        Self {
+            ep,
+            buf: PageBox::new([0; 4]),
+        }
+    }
+
+    async fn get_packet(&mut self) {
+        self.issue_normal_trb().await;
+    }
+
+    async fn issue_normal_trb(&mut self) {
+        for e in &mut self.ep {
+            if e.ty() == EndpointType::InterruptIn {
+                e.issue_normal_trb(&self.buf).await;
+            }
+        }
+    }
+
+    fn print_buf(&self) {
+        info!("{:?}", self.buf);
+    }
+}
