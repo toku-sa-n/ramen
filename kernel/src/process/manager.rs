5d2611c2c945bbcf57bcec163b1686fbfb1d417d
toku-sa-n <tokusan441@gmail.com>
2020-12-21 14:34:46 +0900

    chore: define `switch_process`

--- kernel/src/process/manager.rs
+++ kernel/src/process/manager.rs
@@ -4,6 +4,7 @@ use super::Process;
 use alloc::collections::VecDeque;
 use conquer_once::spin::Lazy;
 use spinning_top::Spinlock;
+use x86_64::VirtAddr;
 
 static MANAGER: Lazy<Spinlock<Manager>> = Lazy::new(|| Spinlock::new(Manager::new()));
 
@@ -15,5 +16,15 @@ impl Manager {
         Self {
             tasks: VecDeque::new(),
         }
+    }
+
+    fn switch_process(rsp: u64) -> u64 {
+        let mut m = MANAGER.lock();
+        m.tasks[0].rsp = VirtAddr::new(rsp);
+
+        let t = m.tasks.pop_front().unwrap();
+        m.tasks.push_back(t);
+
+        m.tasks[0].rsp.as_u64()
     }
 }
