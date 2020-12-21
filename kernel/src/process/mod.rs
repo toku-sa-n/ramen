c8e8a36a71489c3209c6e8fb4f530f3ab9d06eba
toku-sa-n <tokusan441@gmail.com>
2020-12-21 21:54:40 +0900

    refactor: remove `switch_context` function
    Later I will rewrite it.

--- kernel/src/process/mod.rs
+++ kernel/src/process/mod.rs
@@ -38,47 +38,6 @@ fn task_b() {
     loop {}
 }
 
-/// Safety: `current_rsp` must be a valid pointer to RSP value.
-unsafe fn switch_context(current_rsp: *mut u64, old_rsp: u64) {
-    asm!("
-    # Save general registers
-        push rbp
-        push r15
-        push r14
-        push r13
-        push r12
-        push r11
-        push r10
-        push r9
-        push r8
-        push rdi
-        push rsi
-        push rdx
-        push rcx
-        push rax
-
-    # Save the current rsp
-        mov [{}], rsp
-    # switch rsp
-        mov rsp, {}
-
-    # Restore general registers
-        pop rax
-        pop rcx
-        pop rdx
-        pop rsi
-        pop rdi
-        pop r8
-        pop r9
-        pop r10
-        pop r11
-        pop r12
-        pop r13
-        pop r14
-        pop r15
-        pop rbp", out(reg) *current_rsp, in(reg) old_rsp);
-}
-
 pub struct Process {
     pml4: PageBox<PageTable>,
     rip: VirtAddr,
