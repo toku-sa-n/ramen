553d99539f927ae7062e71c13b63a2e485e253a1
toku-sa-n <tokusan441@gmail.com>
2020-12-22 12:36:50 +0900

    refactor: rename to `stack_bottom_addr`
    The name `rsp` is ambiguous.

--- kernel/src/process/mod.rs
+++ kernel/src/process/mod.rs
@@ -44,11 +44,11 @@ pub struct Process {
 impl Process {
     fn new(f: fn()) -> Self {
         let stack = PageBox::new_slice(0, Size4KiB::SIZE.try_into().unwrap());
+        let stack_bottom_addr = stack.virt_addr() + stack.bytes().as_usize();
         let rip = VirtAddr::new((f as usize).try_into().unwrap());
-        let rsp = stack.virt_addr() + stack.bytes().as_usize();
         Self {
             stack,
-            stack_frame: PageBox::new(StackFrame::new(rip, rsp)),
+            stack_frame: PageBox::new(StackFrame::new(rip, stack_bottom_addr)),
         }
     }
 
