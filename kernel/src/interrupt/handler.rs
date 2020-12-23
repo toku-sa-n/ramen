9f26190d80648a9f460a5d4fa40449a14ef42173
toku-sa-n <tokusan441@gmail.com>
2020-12-23 11:58:07 +0900

    fix: forgot to send EOI

--- kernel/src/interrupt/handler.rs
+++ kernel/src/interrupt/handler.rs
@@ -82,6 +82,8 @@ pub extern "x86-interrupt" fn h_14(
 pub extern "x86-interrupt" fn h_20(
     _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
 ) {
+    apic::local::end_of_interrupt();
+
     // Here, the stack pointer points the stack frame of the current task. By cloberring registers,
     // the state will be stored on the stack frame.
     unsafe {
@@ -93,7 +95,6 @@ pub extern "x86-interrupt" fn h_20(
             mov rsp, rax
         ", const INTERRUPT_STACK.as_u64(), sym process::switch, out("rax") _, out("rbx") _, out("rcx") _, out("rdx") _, out("rsi") _, out("rdi") _,  out("r8") _, out("r9") _, out("r10") _, out("r11") _, out("r12") _, out("r13") _, out("r14") _, out("r15") _);
     }
-    apic::local::end_of_interrupt();
 }
 
 pub extern "x86-interrupt" fn h_21(
