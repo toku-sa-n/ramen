c3cd2c640f83a9426b94736e49b8f80781f3498a
toku-sa-n <tokusan441@gmail.com>
2020-12-25 11:41:51 +0900

    chore: define `Spawner::spawn`

--- kernel/src/device/pci/xhci/port/spawner.rs
+++ kernel/src/device/pci/xhci/port/spawner.rs
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use alloc::sync::Arc;
+use multitask::task::Task;
 use spinning_top::Spinlock;
 
 use crate::{
     device::pci::xhci::exchanger::{command, receiver::Receiver},
-    Futurelock,
+    multitask, Futurelock,
 };
+
+use super::Port;
 
 struct Spawner {
     sender: Arc<Futurelock<command::Sender>>,
@@ -15,5 +18,14 @@ struct Spawner {
 impl Spawner {
     fn new(sender: Arc<Futurelock<command::Sender>>, receiver: Arc<Spinlock<Receiver>>) -> Self {
         Self { sender, receiver }
+    }
+
+    fn spawn(&self, port_idx: u8) {
+        let p = Port::new(port_idx);
+        multitask::add(Task::new(super::task(
+            p,
+            self.sender.clone(),
+            self.receiver.clone(),
+        )));
     }
 }
