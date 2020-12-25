6d13c706cb6177f076b182dbd38332e8d07a8063
toku-sa-n <tokusan441@gmail.com>
2020-12-25 12:31:17 +0900

    chore: define `spawned` method
    To check if a task for the port is spawnable.

--- kernel/src/device/pci/xhci/port/spawner.rs
+++ kernel/src/device/pci/xhci/port/spawner.rs
@@ -58,6 +58,10 @@ impl Spawner {
     }
 }
 
+fn spawned(p: &Port) -> bool {
+    SPAWN_STATUS.lock()[usize::from(p.index)]
+}
+
 fn mark_as_spawned(p: &Port) {
     SPAWN_STATUS.lock()[usize::from(p.index)] = true;
 }
