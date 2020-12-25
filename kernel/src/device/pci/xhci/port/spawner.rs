b74ba73c3afa05d2ee4e38228c5c1cb3343dbfcd
toku-sa-n <tokusan441@gmail.com>
2020-12-25 11:35:07 +0900

    chore: define `Spawner` struct

--- kernel/src/device/pci/xhci/port/spawner.rs
+++ kernel/src/device/pci/xhci/port/spawner.rs
@@ -1,1 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use alloc::sync::Arc;
+use spinning_top::Spinlock;
+
+use crate::{
+    device::pci::xhci::exchanger::{command, receiver::Receiver},
+    Futurelock,
+};
+
+struct Spawner {
+    sender: Arc<Futurelock<command::Sender>>,
+    receiver: Arc<Spinlock<Receiver>>,
+}
+impl Spawner {
+    fn new(sender: Arc<Futurelock<command::Sender>>, receiver: Arc<Spinlock<Receiver>>) -> Self {
+        Self { sender, receiver }
+    }
+}
