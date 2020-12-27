a2e40b23fa0317496e343d90da70b49c93d34d32
toku-sa-n <tokusan441@gmail.com>
2020-12-27 13:40:32 +0900

    chore: define `max_scratchpad_buffers`

--- kernel/src/device/pci/xhci/structures/scratchpad.rs
+++ kernel/src/device/pci/xhci/structures/scratchpad.rs
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use crate::mem::allocator::page_box::PageBox;
+use crate::{device::pci::xhci, mem::allocator::page_box::PageBox};
 use conquer_once::spin::OnceCell;
 use x86_64::PhysAddr;
 
 static BUFFER_ARRAY: OnceCell<BufferArray> = OnceCell::uninit();
 
 struct BufferArray(PageBox<PhysAddr>);
+
+fn max_scratchpad_buffers() -> u32 {
+    xhci::handle_registers(|r| r.capability.hcs_params_2.read().max_scratchpad_bufs())
+}
