c4b1fd77d0e1fb0519d14dae10d141e509e0b37c
toku-sa-n <tokusan441@gmail.com>
2020-12-27 13:25:54 +0900

    chore: define `BUFFER_ARRAY` static

--- kernel/src/device/pci/xhci/structures/scratchpad.rs
+++ kernel/src/device/pci/xhci/structures/scratchpad.rs
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use crate::mem::allocator::page_box::PageBox;
+use conquer_once::spin::OnceCell;
 use x86_64::PhysAddr;
+
+static BUFFER_ARRAY: OnceCell<BufferArray> = OnceCell::uninit();
 
 struct BufferArray(PageBox<PhysAddr>);
