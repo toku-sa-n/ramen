b42aca4a9c137c327ee5dc3caeea660561b9716f
toku-sa-n <tokusan441@gmail.com>
2020-12-28 18:58:17 +0900

    refactor: split into two functions

--- kernel/src/mem/allocator/mod.rs
+++ kernel/src/mem/allocator/mod.rs
@@ -40,9 +40,16 @@ pub fn allocate_pages(num_of_pages: NumOfPages<Size4KiB>) -> VirtAddr {
 }
 
 pub fn deallocate_pages(virt: VirtAddr, num_of_pages: NumOfPages<Size4KiB>) {
+    deallocate_phys(virt);
+    deallocate_virt(virt, num_of_pages);
+}
+
+fn deallocate_phys(virt: VirtAddr) {
     let phys = PML4.lock().translate_addr(virt).unwrap();
     FRAME_MANAGER.lock().free(phys);
+}
 
+fn deallocate_virt(virt: VirtAddr, num_of_pages: NumOfPages<Size4KiB>) {
     for i in 0..u64::try_from(num_of_pages.as_usize()).unwrap() {
         let page = Page::<Size4KiB>::from_start_address(virt + Size4KiB::SIZE * i).unwrap();
 
