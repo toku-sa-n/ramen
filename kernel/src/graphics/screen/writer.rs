79676a82f34f39c4141c3a0f2e2e7fbec45659bc
toku-sa-n <tokusan441@gmail.com>
2020-12-30 11:27:13 +0900

    chore: add `id` as a member of `Writer`

--- kernel/src/graphics/screen/writer.rs
+++ kernel/src/graphics/screen/writer.rs
@@ -1,18 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use super::{font, Vram};
+use super::{font, layer, Vram};
 use core::convert::TryFrom;
 use rgb::RGB8;
+use screen_layer::Layer;
 use vek::Vec2;
 
 pub struct Writer {
+    id: screen_layer::Id,
     coord: Vec2<i32>,
     color: RGB8,
 }
 
 impl Writer {
-    pub const fn new(coord: Vec2<i32>, color: RGB8) -> Self {
-        Self { coord, color }
+    pub fn new(coord: Vec2<i32>, color: RGB8) -> Self {
+        let l = Layer::new(Vec2::zero(), Vram::resolution().as_());
+        let id = layer::add(l);
+
+        Self { id, coord, color }
     }
 
     fn print_str(&mut self, str: &str) {
