535743c2ba9a069c820ddc35aa7d04f51210f1a3
toku-sa-n <tokusan441@gmail.com>
2021-01-04 21:00:39 +0900

    chore: define `PIDS`

--- kernel/src/process/collections.rs
+++ kernel/src/process/collections.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use super::Process;
-use alloc::collections::BTreeMap;
+use alloc::collections::{BTreeMap, VecDeque};
+use conquer_once::spin::Lazy;
 
 static PROCESSES: BTreeMap<super::Id, Process> = BTreeMap::new();
+static PIDS: Lazy<VecDeque<super::Id>> = Lazy::new(|| VecDeque::new());
