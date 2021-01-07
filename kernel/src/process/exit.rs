0b39a9049458f03b7318efc2f04d1fbda9318e58
toku-sa-n <tokusan441@gmail.com>
2021-01-07 20:58:09 +0900

    fix: stack was not switched when a process is terminated

--- kernel/src/process/exit.rs
+++ kernel/src/process/exit.rs
@@ -4,8 +4,22 @@ use super::{
     collections,
     manager::{self, Message},
 };
+use common::constant::INTERRUPT_STACK;
+
+// Do not define this as a function as the function cannot return.
+macro_rules! change_stack {
+    () => {
+        const RSP: u64 = INTERRUPT_STACK.as_u64();
+        unsafe {
+            asm!("
+        mov rsp, {}
+        ", const RSP);
+        }
+    };
+}
 
 pub fn exit() -> ! {
+    change_stack!();
     free_stack();
     manager::set_temporary_stack_frame();
     send_exit_message();
