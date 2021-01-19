a28c7315a7a2ea2f778c3d5dfe588ce188077edb
toku-sa-n <tokusan441@gmail.com>
2021-01-19 17:17:51 +0900

    refactor: assert some error cases

--- kernel/src/device/pci/xhci/port/class_driver/mass_storage/mod.rs
+++ kernel/src/device/pci/xhci/port/class_driver/mass_storage/mod.rs
@@ -5,7 +5,10 @@ mod scsi;
 use crate::device::pci::xhci::{port::endpoint, structures::context::EndpointType};
 use core::convert::TryFrom;
 use page_box::PageBox;
-use scsi::{CommandBlockWrapper, CommandBlockWrapperHeaderBuilder, CommandDataBlock, Inquiry};
+use scsi::{
+    CommandBlockWrapper, CommandBlockWrapperHeaderBuilder, CommandDataBlock, CommandStatus,
+    Inquiry, RawInquiry,
+};
 
 pub async fn task(eps: endpoint::Collection) {
     let mut m = MassStorage::new(eps);
@@ -23,7 +26,7 @@ impl MassStorage {
     }
 
     async fn inquiry(&mut self) -> Result<Inquiry, scsi::Invalid> {
-        let b = PageBox::new([0_u8; 36]);
+        let mut b = PageBox::new(CommandStatus::<RawInquiry>::default());
         let header = CommandBlockWrapperHeaderBuilder::default()
             .transfer_length(36)
             .flags(0x80)
@@ -32,20 +35,41 @@ impl MassStorage {
             .build()
             .expect("Failed to build an inquiry command block wrapper.");
         let data = CommandDataBlock::inquiry();
-        let wrapper = PageBox::new(CommandBlockWrapper::new(header, data));
+        let mut wrapper = PageBox::new(CommandBlockWrapper::new(header, data));
 
+        self.send_scsi_command(&mut wrapper, &mut b).await;
+
+        info!("Status: {:?}", b.wrapper().status());
+        let b: CommandStatus<Inquiry> = b.try_into()?.clone();
+        Ok(b.status())
+    }
+
+    async fn send_scsi_command<T: Copy>(
+        &mut self,
+        c: &mut PageBox<CommandBlockWrapper>,
+        recv: &mut PageBox<CommandStatus<T>>,
+    ) {
+        self.send_command_block_wrapper(c).await;
+        self.receive_command_status(recv).await;
+    }
+
+    async fn send_command_block_wrapper(&mut self, c: &mut PageBox<CommandBlockWrapper>) {
         for ep in &mut self.eps {
             if ep.ty() == EndpointType::BulkOut {
-                ep.issue_normal_trb(&wrapper).await;
+                return ep.issue_normal_trb(&c).await;
             }
         }
 
+        unreachable!("MSD class must have at least one Bulk Out endpoint.");
+    }
+
+    async fn receive_command_status<T: Copy>(&mut self, c: &mut PageBox<CommandStatus<T>>) {
         for ep in &mut self.eps {
             if ep.ty() == EndpointType::BulkIn {
-                ep.issue_normal_trb(&b).await;
+                return ep.issue_normal_trb(&c).await;
             }
         }
 
-        Inquiry::try_from(*b)
+        unreachable!("MSD class must have at least one Bulk In endpoint.");
     }
 }
--- kernel/src/device/pci/xhci/port/class_driver/mass_storage/scsi.rs
+++ kernel/src/device/pci/xhci/port/class_driver/mass_storage/scsi.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use core::convert::TryFrom;
+use core::convert::{TryFrom, TryInto};
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 
 #[repr(C, packed)]
 pub(super) struct CommandBlockWrapper {
@@ -42,13 +44,94 @@ impl CommandDataBlock {
     }
 }
 
+#[repr(C, packed)]
+pub(super) struct CommandStatus<T>
+where
+    T: Copy,
+{
+    status: T,
+    wrapper: CommandStatusWrapper,
+}
+impl<T> CommandStatus<T>
+where
+    T: Copy,
+{
+    pub(super) fn status(self) -> T {
+        self.status
+    }
+
+    pub(super) fn wrapper(self) -> CommandStatusWrapper {
+        self.wrapper
+    }
+
+    // Because of the blanket implementation, `impl<T,U> TryFrom<CommandStatus<U>> for
+    // CommandStatus<T> where T:TryFrom<U>` cannot be implemented.
+    pub(super) fn try_into<U>(self) -> Result<CommandStatus<U>, <U as TryFrom<T>>::Error>
+    where
+        U: TryFrom<T> + Copy,
+    {
+        Ok(CommandStatus {
+            status: U::try_from(self.status)?,
+            wrapper: self.wrapper,
+        })
+    }
+}
+impl<T> Default for CommandStatus<T>
+where
+    T: Copy + Default,
+{
+    fn default() -> Self {
+        Self {
+            status: T::default(),
+            wrapper: CommandStatusWrapper::default(),
+        }
+    }
+}
+impl<T> Clone for CommandStatus<T>
+where
+    T: Copy,
+{
+    fn clone(&self) -> Self {
+        CommandStatus {
+            status: self.status,
+            wrapper: self.wrapper,
+        }
+    }
+}
+impl<T> Copy for CommandStatus<T> where T: Copy {}
+
+#[repr(C, packed)]
+#[derive(Copy, Clone, Default)]
+pub(super) struct CommandStatusWrapper {
+    signature: u32,
+    tag: u32,
+    data_residue: u32,
+    status: u8,
+}
+impl CommandStatusWrapper {
+    pub(super) fn status(&self) -> Result<Status, Invalid> {
+        FromPrimitive::from_u8(self.status).ok_or(Invalid::Status(self.status))
+    }
+}
+
+#[derive(Copy, Clone, Debug, FromPrimitive)]
+pub(super) enum Status {
+    Good = 0,
+}
+impl Default for Status {
+    fn default() -> Self {
+        Self::Good
+    }
+}
+
 #[repr(transparent)]
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 pub(super) struct Inquiry([u8; 36]);
-impl TryFrom<[u8; 36]> for Inquiry {
+impl TryFrom<RawInquiry> for Inquiry {
     type Error = Invalid;
 
-    fn try_from(value: [u8; 36]) -> Result<Self, Self::Error> {
+    fn try_from(value: RawInquiry) -> Result<Self, Self::Error> {
+        let value = value.0;
         let peripheral_device_type = value[0] & 0b1_1111;
         let peripheral_qualifier = value[0] >> 5;
         let response_data_format = value[3] & 0b1111;
@@ -65,9 +148,19 @@ impl TryFrom<[u8; 36]> for Inquiry {
     }
 }
 
+#[repr(transparent)]
+#[derive(Copy, Clone)]
+pub(super) struct RawInquiry([u8; 36]);
+impl Default for RawInquiry {
+    fn default() -> Self {
+        Self([0; 36])
+    }
+}
+
 #[derive(Debug)]
 pub(super) enum Invalid {
     PeripheralDeviceType(u8),
     PeripheralQualifier(u8),
     ResponseDataFormat(u8),
+    Status(u8),
 }
