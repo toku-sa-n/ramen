7103fbfc68ab39a1c2a23dc933f12fbddf2dc1af
toku-sa-n <tokusan441@gmail.com>
2021-02-19 12:19:09 +0900

    chore: remove the unused interrupt handlers
    Stopping QEMU by the unhandled interrupts is easier to debug.

--- kernel/src/interrupt/handler.rs
+++ kernel/src/interrupt/handler.rs
@@ -7,78 +7,6 @@ use crate::{
 };
 use common::constant::{INTERRUPT_STACK, PORT_KEY_DATA};
 
-pub extern "x86-interrupt" fn h_00(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("Divide-by-zero Error!");
-}
-
-pub extern "x86-interrupt" fn h_01(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("Debug exception!");
-}
-
-pub extern "x86-interrupt" fn h_02(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("Non-maskable Interrupt!");
-}
-
-pub extern "x86-interrupt" fn h_03(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("Breakpoint!");
-}
-
-pub extern "x86-interrupt" fn h_04(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("Overflow!");
-}
-
-pub extern "x86-interrupt" fn h_05(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("Bound Range Exceeded!");
-}
-
-pub extern "x86-interrupt" fn h_06(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("Invalid Opcode!");
-}
-
-pub extern "x86-interrupt" fn h_07(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("Device Not Available!");
-}
-
-pub extern "x86-interrupt" fn h_09(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("Coprocessor Segment Overrun!");
-}
-
-pub extern "x86-interrupt" fn h_10(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("x87 Floating-Point Exception");
-}
-
-pub extern "x86-interrupt" fn h_13(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("SIMD Floating-Point Exception");
-}
-
-pub extern "x86-interrupt" fn h_14(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    panic!("Virtualization Exception!");
-}
-
 pub extern "x86-interrupt" fn h_20(
     _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
 ) {
@@ -112,10 +40,4 @@ pub extern "x86-interrupt" fn h_2c(
     apic::local::end_of_interrupt();
     let mut port = PORT_KEY_DATA;
     mouse::enqueue_packet(unsafe { port.read() });
-}
-
-pub extern "x86-interrupt" fn h_40(
-    _stack_frame: &mut x86_64::structures::idt::InterruptStackFrame,
-) {
-    info!("Interrupt from 0x40");
 }
