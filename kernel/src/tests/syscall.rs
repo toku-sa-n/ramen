aee3c6a965acb0b7093d2c05d6ee8eb389d09fe3
toku-sa-n <tokusan441@gmail.com>
2021-02-19 19:52:45 +0900

    chore: remove the test for the write system call
    This hangs but we cannot figure out why. QEMU's debug monitor does not
    respond. Also, this system call will be called so many times so if a
    problem happens we can notice it immediately.

--- kernel/src/tests/syscall.rs
+++ kernel/src/tests/syscall.rs
@@ -1,23 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use core::convert::TryInto;
 use page_box::PageBox;
 
 pub(super) fn main() {
     test_translate_address();
-    test_write();
 }
 
 fn test_translate_address() {
     let p = PageBox::from(0_i32);
     assert_eq!(p.phys_addr(), syscalls::translate_address(p.virt_addr()));
-}
-
-fn test_write() {
-    let s = "Test for the write system call";
-    let nbyte = s.len();
-    let buf = s.as_ptr();
-
-    // SAFETY: `buf` is valid.
-    unsafe { syscalls::write(1, buf.cast(), nbyte.try_into().unwrap()) };
 }
