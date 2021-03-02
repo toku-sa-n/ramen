45d74ccbbd81f9e955e8d7e83529bd7d9da30cfd
toku-sa-n <tokusan441@gmail.com>
2021-03-02 21:11:00 +0900

    chore: remove the unused `self`

--- kernel/src/process/ipc.rs
+++ kernel/src/process/ipc.rs
@@ -29,14 +29,14 @@ impl Sender {
     }
 
     fn send(self) {
-        if self.is_receiver_waiting() {
+        if Self::is_receiver_waiting() {
             self.copy_msg_and_wake();
         } else {
             self.set_msg_buf_and_sleep();
         }
     }
 
-    fn is_receiver_waiting(&self) -> bool {
+    fn is_receiver_waiting() -> bool {
         collections::process::handle_running(|p| p.waiting_message() && p.msg_ptr.is_some())
     }
 
