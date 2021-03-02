52fad378d4703b30413ae4f4de7a4050b1edb8e4
toku-sa-n <tokusan441@gmail.com>
2021-03-02 23:21:58 +0900

    chore: add the `must_use` attribute

--- message/src/lib.rs
+++ message/src/lib.rs
@@ -11,6 +11,7 @@ pub struct Message {
 }
 impl Message {
     #[allow(clippy::too_many_arguments)]
+    #[must_use]
     pub fn new(m1: u64, m2: u64, m3: u64, m4: u64) -> Self {
         Self { m1, m2, m3, m4 }
     }
