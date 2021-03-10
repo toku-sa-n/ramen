9538ba7d548974e33f5177d4e1874f08fcefafb3
toku-sa-n <tokusan441@gmail.com>
2021-03-10 16:10:43 +0900

    chore: fetch the capacity of a storage

--- kernel/src/device/pci/xhci/port/class_driver/mass_storage/scsi/response.rs
+++ kernel/src/device/pci/xhci/port/class_driver/mass_storage/scsi/response.rs
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use byteorder::{BigEndian, ByteOrder};
+use core::fmt;
+
 #[derive(Copy, Clone, Debug)]
 #[repr(transparent)]
 pub(in crate::device::pci::xhci) struct Inquiry([u8; 36]);
@@ -9,9 +12,29 @@ impl Default for Inquiry {
     }
 }
 
-#[derive(Copy, Clone, Debug, Default)]
-#[repr(transparent)]
-pub(in crate::device::pci::xhci) struct ReadCapacity10([u8; 8]);
+#[derive(Copy, Clone, Default)]
+#[repr(C)]
+pub(in crate::device::pci::xhci) struct ReadCapacity10 {
+    lba: [u8; 4],
+    block_size: [u8; 4],
+}
+impl ReadCapacity10 {
+    fn lba(self) -> u32 {
+        BigEndian::read_u32(&self.lba)
+    }
+
+    fn block_size(self) -> u32 {
+        BigEndian::read_u32(&self.block_size)
+    }
+}
+impl fmt::Debug for ReadCapacity10 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ReadCapacity10")
+            .field("lba", &self.lba())
+            .field("block_size", &self.block_size())
+            .finish()
+    }
+}
 
 #[derive(Copy, Clone, Debug)]
 #[repr(transparent)]
