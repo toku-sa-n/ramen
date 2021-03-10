ef65de13baa5fb228f7642347d7517d1980302d7
toku-sa-n <tokusan441@gmail.com>
2021-03-10 11:17:39 +0900

    refactor: define `Command` enum

--- kernel/src/device/pci/xhci/port/class_driver/mass_storage/scsi/mod.rs
+++ kernel/src/device/pci/xhci/port/class_driver/mass_storage/scsi/mod.rs
@@ -39,29 +39,46 @@ pub(super) struct CommandDataBlock([u8; 16]);
 impl CommandDataBlock {
     pub(super) fn inquiry() -> Self {
         let mut b = Self::default();
-        b.0[0] = 0x12;
+        b.set_command(Command::Inquiry);
         b.0[4] = 0x24;
         b
     }
 
     pub(super) fn read_capacity() -> Self {
         let mut b = Self::default();
-        b.0[0] = 0x25;
+        b.set_command(Command::ReadCapacity);
         b
     }
 
     pub(super) fn read10() -> Self {
         let mut b = Self::default();
-        b.0[0] = 0x28;
+        b.set_command(Command::Read10);
         b.0[8] = 0x40;
         b
     }
 
     pub(super) fn write10() -> Self {
         let mut b = Self::default();
-        b.0[0] = 0x2a;
+        b.set_command(Command::Write10);
         b.0[8] = 0x40;
         b
+    }
+
+    fn set_command(&mut self, c: Command) {
+        self.0[0] = c.into();
+    }
+}
+
+#[repr(u8)]
+enum Command {
+    Inquiry = 0x12,
+    ReadCapacity = 0x25,
+    Read10 = 0x28,
+    Write10 = 0x2a,
+}
+impl From<Command> for u8 {
+    fn from(c: Command) -> Self {
+        c as u8
     }
 }
 
