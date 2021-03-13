10ea48e2a890462fd72ff13d3b39c78abc73ba77
toku-sa-n <tokusan441@gmail.com>
2021-03-14 00:24:31 +0900

    refactor: split each argument

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-flags="-drive if=pflash,format=raw,file=OVMF_CODE.fd,readonly=on -drive if=pflash,format=raw,file=OVMF_VARS.fd,readonly=on -drive format=raw,file=build/ramen_os.img -no-reboot -m 4G -d int -device isa-debug-exit,iobase=0xf4,iosize=0x04 -device qemu-xhci,id=xhci -device usb-kbd --trace events=trace.event -device usb-mouse, -drive id=usb,file=build/bootx64.efi,if=none,format=raw -device usb-storage,drive=usb"
+flags="
+-drive if=pflash,format=raw,file=OVMF_CODE.fd,readonly=on
+-drive if=pflash,format=raw,file=OVMF_VARS.fd,readonly=on
+-drive format=raw,file=build/ramen_os.img
+-no-reboot
+-m 4G
+-d int
+-device isa-debug-exit,iobase=0xf4,iosize=0x04
+-device qemu-xhci,id=xhci
+-device usb-kbd
+--trace events=trace.event
+-device usb-mouse,
+-drive id=usb,file=build/bootx64.efi,if=none,format=raw
+-device usb-storage,drive=usb
+"
 
-make build/ramen_os.img
+make build/ramen_os.img -j
 qemu-system-x86_64 $flags -no-shutdown -monitor stdio
