30ef542f0d076a8a831ef38df5ba95aafee96e0f
toku-sa-n <tokusan441@gmail.com>
2021-03-14 00:25:12 +0900

    refactor: reorder parameters

--- run_qemu.sh
+++ run_qemu.sh
@@ -4,16 +4,16 @@ flags="
 -drive if=pflash,format=raw,file=OVMF_CODE.fd,readonly=on
 -drive if=pflash,format=raw,file=OVMF_VARS.fd,readonly=on
 -drive format=raw,file=build/ramen_os.img
--no-reboot
--m 4G
--d int
+-drive id=usb,file=build/bootx64.efi,if=none,format=raw
 -device isa-debug-exit,iobase=0xf4,iosize=0x04
 -device qemu-xhci,id=xhci
 -device usb-kbd
---trace events=trace.event
--device usb-mouse,
--drive id=usb,file=build/bootx64.efi,if=none,format=raw
+-device usb-mouse
 -device usb-storage,drive=usb
+-no-reboot
+-m 4G
+-d int
+--trace events=trace.event
 "
 
 make build/ramen_os.img -j
