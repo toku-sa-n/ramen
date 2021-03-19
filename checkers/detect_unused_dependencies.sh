32ffd11be0c65fb0fec3e4e1a3cb9cd6f87e257f
toku-sa-n <tokusan441@gmail.com>
2021-03-19 13:04:24 +0900

    chore: run checkers in parallel

--- checkers/detect_unused_dependencies.sh
+++ checkers/detect_unused_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -name Cargo.toml -printf '%h\n'|xargs -I {} sh -c "cd {} && cargo udeps || exit 255"
+find . -name Cargo.toml -printf '%h\n'|xargs -P 5 -I {} sh -c "cd {} && cargo udeps || exit 255"
