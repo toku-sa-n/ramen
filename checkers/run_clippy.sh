f5695a535385598f0aac4be42d36c7b052d6610c
toku-sa-n <tokusan441@gmail.com>
2021-03-19 13:15:28 +0900

    chore: run 2 processes in parallel
    5 is too heavy for the machines on GitHub Actions.

--- checkers/run_clippy.sh
+++ checkers/run_clippy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -name Cargo.toml -printf '%h\n'|xargs -P 5 -I {} sh -c "cd {} && cargo clippy -- -D clippy::pedantic -D clippy::all || exit 255"
+find . -name Cargo.toml -printf '%h\n'|xargs -P 2 -I {} sh -c "cd {} && cargo clippy -- -D clippy::pedantic -D clippy::all || exit 255"
