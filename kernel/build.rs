edb77bfa9552829eca93615d6b0e389b99405901
toku-sa-n <tokusan441@gmail.com>
2021-03-23 12:18:33 +0900

    fix: create a temporary file before building
    On CI `cargo udeps` and other commands fails because the cpio file is
    not created before running these commands. To solve this, a temporary
    cpio file is created while the build of the kernel.

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,3 +1,14 @@
+use std::process::Command;
+
 fn main() {
+    touch_initrd().expect("Failed to `touch` the initrd cpio file.");
     cc::Build::new().file("src/initrd.s").compile("initrd");
+}
+
+fn touch_initrd() -> Result<(), std::io::Error> {
+    Command::new("touch")
+        .arg("../build/initrd.cpio")
+        .spawn()?
+        .wait()?;
+    Ok(())
 }
