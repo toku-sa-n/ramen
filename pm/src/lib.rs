b9a981457d73bf1c675068263a26facfe24f9dc7
toku-sa-n <tokusan441@gmail.com>
2021-03-24 11:36:11 +0900

    chore: add lines to handle the `getpid` system call

--- pm/src/lib.rs
+++ pm/src/lib.rs
@@ -2,12 +2,66 @@
 
 #![no_std]
 
+extern crate alloc;
+
+use alloc::collections::BTreeMap;
+use core::convert::TryInto;
+use message::Message;
+use num_traits::FromPrimitive;
+
 const PID: i32 = 1;
+const INITIAL_PROCESS_SLOT_NUMBER: usize = 200;
 
 pub fn main() {
+    let mut processes = BTreeMap::new();
+    init(&mut processes);
+    main_loop(&mut processes);
+}
+
+fn init(processes: &mut BTreeMap<i32, Process>) {
     ensure_pid_is_correct();
+    add_initial_slots(processes);
 }
 
 fn ensure_pid_is_correct() {
     assert_eq!(syscalls::getpid(), PID, "Wrong PID");
+}
+
+fn add_initial_slots(processes: &mut BTreeMap<i32, Process>) {
+    for i in 0..INITIAL_PROCESS_SLOT_NUMBER {
+        processes.insert(i.try_into().unwrap(), Process::new(i.try_into().unwrap()));
+    }
+}
+
+fn main_loop(processes: &mut BTreeMap<i32, Process>) {
+    loop {
+        loop_iteration(processes)
+    }
+}
+
+fn loop_iteration(processes: &mut BTreeMap<i32, Process>) {
+    let m = syscalls::receive_from_any();
+
+    if let Some(syscalls::Ty::GetPid) = FromPrimitive::from_u64(m.body.0) {
+        let pid = processes
+            .get(&m.header.sender)
+            .expect("No such process.")
+            .pid;
+        let h = message::Header::new(PID);
+        let b = message::Body(pid.try_into().unwrap(), 0, 0, 0, 0);
+
+        let reply = Message::new(h, b);
+        let to = m.header.sender;
+
+        syscalls::send(reply, to);
+    }
+}
+
+struct Process {
+    pid: i32,
+}
+impl Process {
+    fn new(pid: i32) -> Self {
+        Self { pid }
+    }
 }
