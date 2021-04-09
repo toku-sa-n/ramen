01e708a990d4df38d12705febaff83d4fc351985
Hiroki Tokunaga <tokusan441@gmail.com>
2021-04-09 21:02:25 +0900

    feat: implement the `print` and `println` macros

--- ralib/src/io.rs
+++ ralib/src/io.rs
@@ -1,1 +1,27 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use alloc::string::ToString;
+use core::{convert::TryInto, fmt};
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::io::_print(core::format_args!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! println{
+    () => {
+        print!("\n");
+    };
+    ($($arg:tt)*)=>{
+        print!("{}\n",core::format_args!($($arg)*));
+    }
+}
+
+pub fn _print(args: fmt::Arguments) {
+    let s = args.to_string();
+
+    unsafe { syscalls::write(1, s.as_ptr().cast(), s.len().try_into().unwrap()) };
+}
