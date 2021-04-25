bc749836463eceaf4ba0898a459ea43054eae8d8
Hiroki Tokunaga <tokusan441@gmail.com>
2021-04-25 18:16:43 +0900

    refactor: remove the unstable const `BTreeMap::new` (#905)

--- kernel/src/process/slot_id.rs
+++ kernel/src/process/slot_id.rs
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use alloc::collections::BTreeSet;
+use conquer_once::spin::Lazy;
 use core::ops::DerefMut;
 use spinning_top::Spinlock;
 
 pub(crate) type SlotId = i32;
 
-static GENERATOR: Spinlock<Generator> = Spinlock::new(Generator::new());
+static GENERATOR: Lazy<Spinlock<Generator>> = Lazy::new(|| Spinlock::new(Generator::new()));
 
 pub(super) fn generate() -> SlotId {
     lock_generator().generate()
@@ -21,7 +22,7 @@ struct Generator {
     used_ids: BTreeSet<SlotId>,
 }
 impl Generator {
-    const fn new() -> Self {
+    fn new() -> Self {
         Self {
             used_ids: BTreeSet::new(),
         }
