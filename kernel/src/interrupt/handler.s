0a20082ea3a0ae2a20358053964c019475d9d2b4
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-11 18:01:32 +0900

    refactor(handler.s): use a macro

--- kernel/src/interrupt/handler.s
+++ kernel/src/interrupt/handler.s
@@ -9,9 +9,10 @@
     .text
     .code64
 
-    .global h_20_asm
+    .macro handler name
+    .global \name\()_asm
     .extern h_20
-h_20_asm:
+    \name\()_asm:
     push rbp
     mov rbp, rsp
     push r15
@@ -30,7 +31,7 @@ h_20_asm:
     push rax
 
     mov rsp, INTERRUPT_STACK
-    call h_20
+    call \name
     mov rsp, rax
 
     pop rax
@@ -50,87 +51,8 @@ h_20_asm:
 
     pop rbp
     iretq
+    .endm
 
-    .global h_80_asm
-    .extern h_80
-h_80_asm:
-    push rbp
-    mov rbp, rsp
-    push r15
-    push r14
-    push r13
-    push r12
-    push r11
-    push r10
-    push r9
-    push r8
-    push rdi
-    push rsi
-    push rdx
-    push rcx
-    push rbx
-    push rax
-
-    mov rsp, INTERRUPT_STACK
-    call h_80
-    mov rsp, rax
-
-    pop rax
-    pop rbx
-    pop rcx
-    pop rdx
-    pop rsi
-    pop rdi
-    pop r8
-    pop r9
-    pop r10
-    pop r11
-    pop r12
-    pop r13
-    pop r14
-    pop r15
-
-    pop rbp
-    iretq
-
-    .global h_81_asm
-    .extern h_81
-h_81_asm:
-    push rbp
-    mov rbp, rsp
-    push r15
-    push r14
-    push r13
-    push r12
-    push r11
-    push r10
-    push r9
-    push r8
-    push rdi
-    push rsi
-    push rdx
-    push rcx
-    push rbx
-    push rax
-
-    mov rsp, INTERRUPT_STACK
-    call h_81
-    mov rsp, rax
-
-    pop rax
-    pop rbx
-    pop rcx
-    pop rdx
-    pop rsi
-    pop rdi
-    pop r8
-    pop r9
-    pop r10
-    pop r11
-    pop r12
-    pop r13
-    pop r14
-    pop r15
-
-    pop rbp
-    iretq
+    handler h_20
+    handler h_80
+    handler h_81
