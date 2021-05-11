db9258b40018baa4a19e943b6468a1564d9ec613
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-11 17:50:34 +0900

    refactor: remove unnecessary `pub`s

--- kernel/src/interrupt/handler.rs
+++ kernel/src/interrupt/handler.rs
@@ -1,13 +1,13 @@
 use crate::{interrupt::apic::local, process, syscall};
 
 #[no_mangle]
-pub extern "C" fn h_20() -> u64 {
+extern "C" fn h_20() -> u64 {
     local::end_of_interrupt();
     process::switch().as_u64()
 }
 
 #[no_mangle]
-pub extern "C" fn h_80() -> u64 {
+extern "C" fn h_80() -> u64 {
     unsafe { syscall::prepare_arguments() }
     process::assign_rax_from_register();
     local::end_of_interrupt();
@@ -15,7 +15,7 @@ pub extern "C" fn h_80() -> u64 {
 }
 
 #[no_mangle]
-pub extern "C" fn h_81() -> u64 {
+extern "C" fn h_81() -> u64 {
     unsafe { syscall::prepare_arguments() }
     local::end_of_interrupt();
     process::switch().as_u64()
