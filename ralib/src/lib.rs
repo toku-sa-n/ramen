ef3d8e2b7c4201a6c133240406e821e45ae0c25f
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-15 00:34:52 +0900

    chore(ralib): remove the `heap` feature (#939)
    See: #938.

--- ralib/src/lib.rs
+++ ralib/src/lib.rs
@@ -2,23 +2,16 @@
 
 #![no_std]
 #![allow(clippy::too_many_arguments)] // A workaround for the clippy's wrong warning.
-#![cfg_attr(feature = "heap", feature(alloc_error_handler))]
+#![feature(alloc_error_handler)]
 
 pub mod io;
 pub mod mem;
 
-#[cfg(feature = "heap")]
 extern crate alloc;
 
-#[cfg(feature = "heap")]
 pub fn init() {
     io::init();
     mem::heap::init();
-}
-
-#[cfg(not(feature = "heap"))]
-pub fn init() {
-    io::init();
 }
 
 #[panic_handler]
