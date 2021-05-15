5814dc54953f0480a8c5c06a3c09aebb91b2c3e8
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-15 10:15:42 +0900

    Revert "chore(ralib): remove the `heap` feature (#939)"
    This reverts commit ef3d8e2b7c4201a6c133240406e821e45ae0c25f.

--- ralib/src/lib.rs
+++ ralib/src/lib.rs
@@ -2,16 +2,23 @@
 
 #![no_std]
 #![allow(clippy::too_many_arguments)] // A workaround for the clippy's wrong warning.
-#![feature(alloc_error_handler)]
+#![cfg_attr(feature = "heap", feature(alloc_error_handler))]
 
 pub mod io;
 pub mod mem;
 
+#[cfg(feature = "heap")]
 extern crate alloc;
 
+#[cfg(feature = "heap")]
 pub fn init() {
     io::init();
     mem::heap::init();
+}
+
+#[cfg(not(feature = "heap"))]
+pub fn init() {
+    io::init();
 }
 
 #[panic_handler]
