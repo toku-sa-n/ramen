58479218413178d239f515824de02a5497ce62a6
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-15 20:17:56 +0900

    refactor(fm): define `Initializer::init`

--- fm/src/lib.rs
+++ fm/src/lib.rs
@@ -29,7 +29,7 @@ fn init() {
 
 fn sync_with_sysproc() {
     start_sync_with_sysproc();
-    init_frame_manager();
+    Initializer::init();
 }
 
 fn start_sync_with_sysproc() {
@@ -42,34 +42,38 @@ fn start_sync_with_sysproc() {
     );
 }
 
-fn init_frame_manager() {
-    let mut m;
-    let mut v = Vec::new();
+#[derive(Default)]
+struct Initializer(Vec<Frames>);
+impl Initializer {
+    fn init() {
+        let mut m;
+        let mut v = Vec::new();
 
-    while {
-        m = receive_from_sysproc();
+        while {
+            m = receive_from_sysproc();
 
-        !sync_finished(m)
-    } {
-        let start = PhysAddr::new(m.body.1);
-        let num_of_pages = NumOfPages::new(m.body.2.try_into().unwrap());
-        let available = match m.body.3 {
-            0 => false,
-            1 => true,
-            _ => unreachable!("`available` is neither 0 nor 1."),
-        };
+            !sync_finished(m)
+        } {
+            let start = PhysAddr::new(m.body.1);
+            let num_of_pages = NumOfPages::new(m.body.2.try_into().unwrap());
+            let available = match m.body.3 {
+                0 => false,
+                1 => true,
+                _ => unreachable!("`available` is neither 0 nor 1."),
+            };
 
-        let frames = if available {
-            Frames::new_for_available(start, num_of_pages)
-        } else {
-            Frames::new_for_used(start, num_of_pages)
-        };
+            let frames = if available {
+                Frames::new_for_available(start, num_of_pages)
+            } else {
+                Frames::new_for_used(start, num_of_pages)
+            };
 
-        v.push(frames);
+            v.push(frames);
+        }
+
+        let r = FRAME_MANAGER.try_init_once(|| Spinlock::new(v.into()));
+        r.expect("Failed to initialize `FRAME_MANAGER`.");
     }
-
-    let r = FRAME_MANAGER.try_init_once(|| Spinlock::new(v.into()));
-    r.expect("Failed to initialize `FRAME_MANAGER`.");
 }
 
 fn sync_finished(m: Message) -> bool {
