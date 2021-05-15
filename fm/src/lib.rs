f54f01eac22dd7825116701553b84c74079de9ca
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-15 17:32:41 +0900

    feat(fm): start syncing with `sysproc`

--- fm/src/lib.rs
+++ fm/src/lib.rs
@@ -12,4 +12,19 @@ pub fn main() {
 fn init() {
     ralib::init();
     heap::init();
+    sync_with_sysproc();
+}
+
+fn sync_with_sysproc() {
+    start_sync_with_sysproc();
+}
+
+fn start_sync_with_sysproc() {
+    let m = syscalls::receive_from(5);
+
+    assert_eq!(
+        m.body.0,
+        fm_message::Ty::StartInitialization as _,
+        "Failed to receive the Start Initialization message from the sysproc."
+    );
 }
