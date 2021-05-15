e4fef5dedf82f49b436d9c8b0edbc2a914df07c8
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-15 17:02:18 +0900

    feat(sysproc): send frame information to `fm`

--- kernel/src/sysproc.rs
+++ kernel/src/sysproc.rs
@@ -1,3 +1,7 @@
+use core::convert::TryInto;
+
+use crate::mem::allocator::phys;
+use frame_manager::Frames;
 use message::Message;
 
 pub(crate) fn main() {
@@ -9,11 +13,30 @@ pub(crate) fn main() {
 
 fn sync_with_fm() {
     start_sync_with_fm();
+    send_frame_ranges();
 }
 
 fn start_sync_with_fm() {
     let h = message::Header::default();
     let b = message::Body(fm_message::Ty::StartInitialization as _, 0, 0, 0, 0);
+    let m = Message::new(h, b);
+
+    syscalls::send(m, 4);
+}
+
+fn send_frame_ranges() {
+    for f in &phys::frames() {
+        send_frame_range(f);
+    }
+}
+
+fn send_frame_range(f: &Frames) {
+    let start = f.start().as_u64();
+    let num: u64 = f.num_of_pages().as_usize().try_into().unwrap();
+    let availble = f.available();
+
+    let h = message::Header::default();
+    let b = message::Body(fm_message::Ty::AddFrames as _, start, num, availble as _, 0);
     let m = Message::new(h, b);
 
     syscalls::send(m, 4);
