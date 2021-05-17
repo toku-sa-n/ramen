f8a44a5f3ed8e523de11a26564de41394c8b2c22
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-17 18:07:09 +0900

    chore(vm): define `map`

--- vm/src/page_table/mapper.rs
+++ vm/src/page_table/mapper.rs
@@ -1,6 +1,7 @@
+use crate::frame;
 use conquer_once::spin::Lazy;
 use spinning_top::Spinlock;
-use x86_64::structures::paging::RecursivePageTable;
+use x86_64::structures::paging::{Mapper, Page, PageTableFlags, PhysFrame, RecursivePageTable};
 
 static MAPPER: Lazy<Spinlock<RecursivePageTable>> = Lazy::new(|| {
     let a = 0xff7f_bfdf_e000 as *mut _;
@@ -10,3 +11,12 @@ static MAPPER: Lazy<Spinlock<RecursivePageTable>> = Lazy::new(|| {
 
     Spinlock::new(t)
 });
+
+fn map(page: Page, frame: PhysFrame, flags: PageTableFlags) {
+    let m = MAPPER.try_lock();
+    let mut m = m.expect("Failed to lock `MAPPER`");
+
+    let r = unsafe { m.map_to(page, frame, flags, &mut frame::Allocator) };
+    let flush = r.expect("Failed to map a page.");
+    flush.flush();
+}
