7ffbbe42faa4b8be31822f14173b87c85fe7d84f
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-16 17:16:56 +0900

    chore(extended_capability): implement `Iter`

---
+++ xhci/src/pci/config/extended_capability.rs
@@ -0,0 +1,25 @@
+use crate::pci::config::{RegisterIndex, Registers};
+use bit_field::BitField;
+use core::convert::{TryFrom, TryInto};
+
+struct Iter {
+    registers: Registers,
+    index: RegisterIndex,
+}
+impl Iterator for Iter {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let raw = self.registers.get(self.index);
+        let next = raw.get_bits(8..=15);
+
+        if next == 0 {
+            None
+        } else {
+            self.index =
+                RegisterIndex::new(self.index.as_usize() + usize::try_from(next >> 2).unwrap());
+
+            Some(raw.get_bits(0..=7).try_into().unwrap())
+        }
+    }
+}
