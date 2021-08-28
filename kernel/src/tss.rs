bf2e5709119315b71c25b0703a277db4b9d4189b
Hiroki Tokunaga <tokusan441@gmail.com>
2021-08-28 11:10:25 +0900

    refactor(constant): calculate an address

--- kernel/src/tss.rs
+++ kernel/src/tss.rs
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use common::constant::INTERRUPT_STACK;
+use conquer_once::spin::Lazy;
 use spinning_top::Spinlock;
 use x86_64::structures::tss::TaskStateSegment;
 
-pub(crate) static TSS: Spinlock<TaskStateSegment> = {
+pub(crate) static TSS: Lazy<Spinlock<TaskStateSegment>> = Lazy::new(|| {
     let mut tss = TaskStateSegment::new();
-    tss.interrupt_stack_table[0] = INTERRUPT_STACK;
+    tss.interrupt_stack_table[0] = *INTERRUPT_STACK;
     Spinlock::new(tss)
-};
+});
