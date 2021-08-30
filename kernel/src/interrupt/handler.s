17f7b6e6b8c19f9cf86a71295f2f1226762e3379
Hiroki Tokunaga <tokusan441@gmail.com>
2021-08-30 14:23:10 +0900

    style(handler): format

--- kernel/src/interrupt/handler.s
+++ kernel/src/interrupt/handler.s
@@ -1,58 +1,59 @@
-    .intel_syntax noprefix
+.intel_syntax noprefix
 
-    .set INTERRUPT_STACK, 0xffffffffc0000000 - (0x1000 * 16 / 2)
-    .extern end_of_interrupt
-    .extern switch
-    .extern prepare_arguments
-    .extern assign_rax_from_register
+.set    INTERRUPT_STACK, 0xffffffffc0000000 - (0x1000 * 16 / 2)
+.extern end_of_interrupt
+.extern switch
+.extern prepare_arguments
+.extern assign_rax_from_register
 
-    .text
-    .code64
+.text
+.code64
 
-    .macro handler name
-    .global \name\()_asm
-    .extern h_20
-    \name\()_asm:
-    push rbp
-    mov rbp, rsp
-    push r15
-    push r14
-    push r13
-    push r12
-    push r11
-    push r10
-    push r9
-    push r8
-    push rdi
-    push rsi
-    push rdx
-    push rcx
-    push rbx
-    push rax
+.macro  handler name
+.global \name\()_asm
+.extern h_20
 
-    mov rsp, INTERRUPT_STACK
-    call \name
-    mov rsp, rax
+\name\()_asm:
+	push rbp
+	mov  rbp, rsp
+	push r15
+	push r14
+	push r13
+	push r12
+	push r11
+	push r10
+	push r9
+	push r8
+	push rdi
+	push rsi
+	push rdx
+	push rcx
+	push rbx
+	push rax
 
-    pop rax
-    pop rbx
-    pop rcx
-    pop rdx
-    pop rsi
-    pop rdi
-    pop r8
-    pop r9
-    pop r10
-    pop r11
-    pop r12
-    pop r13
-    pop r14
-    pop r15
+	mov  rsp, INTERRUPT_STACK
+	call \name
+	mov  rsp, rax
 
-    pop rbp
-    iretq
-    .endm
+	pop rax
+	pop rbx
+	pop rcx
+	pop rdx
+	pop rsi
+	pop rdi
+	pop r8
+	pop r9
+	pop r10
+	pop r11
+	pop r12
+	pop r13
+	pop r14
+	pop r15
 
-    handler h_20
-    handler h_80
-    handler h_81
+	pop rbp
+	iretq
+	.endm
+
+	handler h_20
+	handler h_80
+	handler h_81
