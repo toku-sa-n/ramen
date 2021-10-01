1dc447c7c711ab5c4b3481b2cebbf10e5b657d6c
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-01 11:05:09 +0900

    chore: update uefi deps (#1029)

--- bootx64/src/init.rs
+++ bootx64/src/init.rs
@@ -5,16 +5,16 @@ use uefi::{
     ResultExt,
 };
 
-pub fn init(system_table: &SystemTable<Boot>) {
+pub fn init(system_table: &mut SystemTable<Boot>) {
     init_uefi_utils(system_table);
     reset_console(system_table);
 }
 
-fn init_uefi_utils(system_table: &SystemTable<Boot>) {
+fn init_uefi_utils(system_table: &mut SystemTable<Boot>) {
     uefi_services::init(system_table).expect_success("Failed to initialize_uefi_utilities");
 }
 
-fn reset_console(system_table: &SystemTable<Boot>) {
+fn reset_console(system_table: &mut SystemTable<Boot>) {
     system_table
         .stdout()
         .reset(false)
