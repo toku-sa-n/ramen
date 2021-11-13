ed32878d54cb2ed110a7ca0a7b30e12c75e0fab7
Hiroki Tokunaga <tokusan441@gmail.com>
2021-11-13 16:21:24 +0900

    chore: do not specify the interrupt stack
    I do not know why I set the interrupt stack, but this is not necessary.

--- kernel/src/interrupt/idt.rs
+++ kernel/src/interrupt/idt.rs
@@ -9,19 +9,15 @@ use {
 static IDT: Lazy<InterruptDescriptorTable> = Lazy::new(|| {
     let mut idt = InterruptDescriptorTable::new();
 
-    unsafe {
-        idt[0x20].set_handler_fn(h_20).set_stack_index(0);
+    idt[0x20].set_handler_fn(h_20);
 
-        idt[0x80]
-            .set_handler_fn(h_80)
-            .set_stack_index(0)
-            .set_privilege_level(PrivilegeLevel::Ring3);
+    idt[0x80]
+        .set_handler_fn(h_80)
+        .set_privilege_level(PrivilegeLevel::Ring3);
 
-        idt[0x81]
-            .set_handler_fn(h_81)
-            .set_stack_index(0)
-            .set_privilege_level(PrivilegeLevel::Ring3);
-    }
+    idt[0x81]
+        .set_handler_fn(h_81)
+        .set_privilege_level(PrivilegeLevel::Ring3);
 
     idt
 });
