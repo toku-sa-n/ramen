36453f1d0d8b4992084e816af2d4e8e1a01cfe97
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-10 09:49:05 +0900

    refactor: remove an unused file

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,7 +1,4 @@
 fn main() {
-    cc::Build::new()
-        .file("src/interrupt/handler.s")
-        .compile("handler");
     cc::Build::new().file("src/asm.s").compile("asm");
     cc::Build::new().file("src/qemu.s").compile("qemu");
 }
