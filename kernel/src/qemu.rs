287879b7c020a8740b7d460ebfd3081ae914be6c
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-11 15:34:35 +0900

    refactor: remove `qemu.s`

--- kernel/src/qemu.rs
+++ kernel/src/qemu.rs
@@ -1,14 +1,16 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+use qemu_exit::QEMUExit;
 
-extern "C" {
-    fn exit_qemu_as_success() -> !;
-    fn exit_qemu_as_failure() -> !;
-}
+const IO_BASE: u16 = 0xf4;
+const EXIT_SUCCESS_CODE: u32 = 33;
 
 pub(crate) fn exit_success() -> ! {
-    unsafe { exit_qemu_as_success() }
+    exit_handler().exit_success();
 }
 
 pub(crate) fn exit_failure() -> ! {
-    unsafe { exit_qemu_as_failure() }
+    exit_handler().exit_failure();
+}
+
+fn exit_handler() -> qemu_exit::X86 {
+    qemu_exit::X86::new(IO_BASE, EXIT_SUCCESS_CODE)
 }
