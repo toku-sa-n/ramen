9e2b370ed17718461a826a4e716e2bc2d89b9756
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-11 16:01:36 +0900

    refactor: remove `asm.s` (#1066)

--- kernel/build.rs
+++ kernel/build.rs
@@ -2,5 +2,4 @@ fn main() {
     cc::Build::new()
         .file("src/interrupt/handler.s")
         .compile("handler");
-    cc::Build::new().file("src/asm.s").compile("asm");
 }
--- kernel/src/interrupt/handler.rs
+++ kernel/src/interrupt/handler.rs
@@ -1,9 +1,5 @@
 use crate::{interrupt::apic::local, process};
 
-extern "C" {
-    fn syscall_prepare_arguments() -> u64;
-}
-
 #[no_mangle]
 extern "C" fn h_20() -> u64 {
     local::end_of_interrupt();
@@ -12,7 +8,7 @@ extern "C" fn h_20() -> u64 {
 
 #[no_mangle]
 extern "C" fn h_80() -> u64 {
-    let v = unsafe { syscall_prepare_arguments() };
+    let v = syscall_prepare_arguments();
     process::scheduler::assign_to_rax(v);
     local::end_of_interrupt();
     process::switch().as_u64()
@@ -20,7 +16,25 @@ extern "C" fn h_80() -> u64 {
 
 #[no_mangle]
 extern "C" fn h_81() -> u64 {
-    unsafe { syscall_prepare_arguments() };
+    syscall_prepare_arguments();
     local::end_of_interrupt();
     process::switch().as_u64()
+}
+
+#[naked]
+extern "sysv64" fn syscall_prepare_arguments() -> u64 {
+    unsafe {
+        asm!(
+            "
+    mov rcx, rdx
+    mov rdx, rsi
+    mov rsi, rdi
+    mov rdi, rax
+
+    call select_proper_syscall
+
+    ret",
+            options(noreturn)
+        )
+    }
 }
