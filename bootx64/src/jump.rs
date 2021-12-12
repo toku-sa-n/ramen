3b5a6d489b5d5fc5448233c60cabd2a5ff6e4b15
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-12 23:39:45 +0900

    chore: remove an unnecessary function
    Even without the function no exceptions happened.

--- bootx64/src/jump.rs
+++ bootx64/src/jump.rs
@@ -1,22 +1,7 @@
 use {predefined_mmap::STACK_BASE, x86_64::VirtAddr};
 
 pub fn to_kernel(mut boot_info: boot_info::Info) -> ! {
-    disable_interruption();
-
     switch_stack_and_call_kernel_code(&mut boot_info, boot_info.entry_addr(), STACK_BASE)
-}
-
-fn disable_interruption() {
-    // Use `nop` because some machines go wrong when continuously doing `out`.
-    unsafe {
-        asm!(
-            "mov al,0xff
-            out 0x21,al
-            nop
-            out 0xa1,al
-            cli"
-        );
-    }
 }
 
 #[naked]
