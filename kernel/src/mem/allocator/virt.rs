ddf9a42406fb9ee4941a3bd5193075a2e79f5eef
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-13 10:53:23 +0900

    refactor: define `map_pages_from`

--- kernel/src/mem/allocator/virt.rs
+++ kernel/src/mem/allocator/virt.rs
@@ -1,47 +1,20 @@
-use predefined_mmap::{BYTES_AVAILABLE_RAM, STACK_BASE};
-
 use {
     crate::mem::paging,
-    core::convert::TryFrom,
     os_units::NumOfPages,
-    predefined_mmap::KERNEL_ADDR,
     x86_64::{
-        structures::paging::{PageSize, Size4KiB},
+        structures::paging::{page::PageRange, Size4KiB},
         VirtAddr,
     },
 };
 
-pub(crate) fn search_free_addr_for_user(num_pages: NumOfPages<Size4KiB>) -> Option<VirtAddr> {
+pub(crate) fn search_free_addr_from(
+    num_pages: NumOfPages<Size4KiB>,
+    region: PageRange,
+) -> Option<VirtAddr> {
     let mut cnt = 0;
     let mut start = None;
-    for addr in (0..KERNEL_ADDR.as_u64()).step_by(usize::try_from(Size4KiB::SIZE).unwrap()) {
-        let addr = VirtAddr::new(addr as _);
-        if available(addr) {
-            if start.is_none() {
-                start = Some(addr);
-            }
-
-            cnt += 1;
-
-            if cnt >= num_pages.as_usize() {
-                return start;
-            }
-        } else {
-            cnt = 0;
-            start = None;
-        }
-    }
-
-    None
-}
-
-pub(crate) fn search_free_addr_for_kernel(num_pages: NumOfPages<Size4KiB>) -> Option<VirtAddr> {
-    let mut cnt = 0;
-    let mut start = None;
-    for addr in (STACK_BASE.as_u64()..BYTES_AVAILABLE_RAM.as_usize().try_into().unwrap())
-        .step_by(usize::try_from(Size4KiB::SIZE).unwrap())
-    {
-        let addr = VirtAddr::new(addr as _);
+    for page in region {
+        let addr = page.start_address();
         if available(addr) {
             if start.is_none() {
                 start = Some(addr);
