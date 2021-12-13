4223429bf9ed9aa42782855c7cdc926588e81d38
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-13 17:56:31 +0900

    chore: use `naked`

--- kernel/src/process/context.rs
+++ kernel/src/process/context.rs
@@ -66,13 +66,87 @@ impl Context {
         )
     }
 
-    pub(super) fn switch(old: *mut Context, new: *mut Context) {
-        extern "sysv64" {
-            fn asm_switch_context(old: *mut Context, new: *mut Context);
-        }
-
+    #[naked]
+    #[allow(clippy::too_many_lines)]
+    pub(super) extern "sysv64" fn switch(old: *mut Context, new: *mut Context) {
         unsafe {
-            asm_switch_context(old, new);
+            asm!(
+                "
+    mov [rdi+0x00], rax
+    mov [rdi+0x08], rbx
+    mov [rdi+0x10], rcx
+    mov [rdi+0x18], rdx
+
+    lea rax, [rsp+0x08]
+    mov [rdi+0x20], rax
+    mov [rdi+0x28], rbp
+    mov [rdi+0x30], rsi
+    mov [rdi+0x38], rdi
+
+    mov [rdi+0x40], r8
+    mov [rdi+0x48], r9
+    mov [rdi+0x50], r10
+    mov [rdi+0x58], r11
+
+    mov [rdi+0x60], r12
+    mov [rdi+0x68], r13
+    mov [rdi+0x70], r14
+    mov [rdi+0x78], r15
+
+    mov [rdi+0x80], cs
+    mov [rdi+0x88], ss
+    mov [rdi+0x90], fs
+    mov [rdi+0x98], gs
+
+    mov rax, cr3
+    mov [rdi+0xa0], rax
+    mov rax, [rsp]
+    mov [rdi+0xa8], rax
+    pushfq
+    pop qword ptr [rdi+0xb0]
+
+    fxsave [rdi+0xc0]
+
+    mov rax, [rsi+0x00]
+    mov rbx, [rsi+0x08]
+    mov rcx, [rsi+0x10]
+    mov rdx, [rsi+0x18]
+
+    mov rbp, [rsi+0x28]
+    mov rdi, [rsi+0x38]
+
+    mov r8, [rsi+0x40]
+    mov r9, [rsi+0x48]
+    mov r10, [rsi+0x50]
+    mov r11, [rsi+0x58]
+
+    mov r12, [rsi+0x60]
+    mov r13, [rsi+0x68]
+    mov r14, [rsi+0x70]
+    mov r15, [rsi+0x78]
+
+    mov rax, [rsi+0x90]
+    mov fs, ax
+    mov rax, [rsi+0x98]
+    mov gs, ax
+
+    mov rax, [rsi+0xa0]
+    mov cr3, rax
+
+    fxrstor [rsi+0xc0]
+
+    push qword ptr [rsi+0x88]
+    push qword ptr [rsi+0x20]
+    push qword ptr [rsi+0xb0]
+    push qword ptr [rsi+0x80]
+    push qword ptr [rsi+0xa8]
+
+    mov rsi, [rsi+0x30]
+
+    iretq
+    ",
+                options(noreturn)
+            );
         }
     }
 
