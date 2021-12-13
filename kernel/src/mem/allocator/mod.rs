4430518de52de1b0f7f8b5de9c6e2ca01f45f511
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-13 11:14:26 +0900

    chore: rename functions

--- kernel/src/mem/allocator/mod.rs
+++ kernel/src/mem/allocator/mod.rs
@@ -14,7 +14,7 @@ pub(crate) mod kpbox;
 pub(crate) mod phys;
 pub(crate) mod virt;
 
-pub(crate) fn allocate_pages(num_of_pages: NumOfPages<Size4KiB>) -> Option<VirtAddr> {
+pub(crate) fn allocate_pages_for_user(num_of_pages: NumOfPages<Size4KiB>) -> Option<VirtAddr> {
     let phys_addr = allocate_phys(num_of_pages)?;
 
     let virt_addr = super::map_pages_for_user(phys_addr, num_of_pages.as_bytes());
