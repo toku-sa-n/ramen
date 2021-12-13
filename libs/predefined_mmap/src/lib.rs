a5cc402b7444e64a52139635fb9f40d9e5b50d39
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-13 10:04:38 +0900

    chore: add functions

--- libs/predefined_mmap/src/lib.rs
+++ libs/predefined_mmap/src/lib.rs
@@ -3,7 +3,10 @@
 use {
     conquer_once::spin::Lazy,
     os_units::{Bytes, NumOfPages},
-    x86_64::{structures::paging::Size4KiB, VirtAddr},
+    x86_64::{
+        structures::paging::{page::PageRange, Page, PageSize, Size4KiB},
+        VirtAddr,
+    },
 };
 
 pub const KERNEL_ADDR: VirtAddr = VirtAddr::new_truncate(0xffff_ffff_8000_0000);
@@ -19,3 +22,39 @@ pub const RECUR_PML4_ADDR: VirtAddr = VirtAddr::new_truncate(0xff7f_bfdf_e000);
 
 pub static NUM_OF_PAGES_STACK: Lazy<NumOfPages<Size4KiB>> = Lazy::new(|| NumOfPages::new(16));
 pub const BYTES_AVAILABLE_RAM: Bytes = Bytes::new(0x1_0000_0000_0000);
+
+pub fn user() -> PageRange {
+    let start = VirtAddr::new(0x1000);
+    let start = Page::from_start_address(start).expect("The address is not page-aligned.");
+
+    let end = VirtAddr::new(0xffff_ffff_8000_0000);
+    let end = Page::from_start_address(end).expect("The address is not page-aligned.");
+
+    PageRange { start, end }
+}
+
+pub fn kernel() -> PageRange {
+    next_to(user(), NumOfPages::new(16))
+}
+
+pub fn stack() -> PageRange {
+    next_to(kernel(), NumOfPages::new(32))
+}
+
+pub fn heap() -> PageRange {
+    next_to(stack(), NumOfPages::new(64))
+}
+
+pub fn kernel_dma() -> PageRange {
+    next_to(heap(), NumOfPages::new(64))
+}
+
+fn next_to<S: PageSize>(range: PageRange<S>, n: NumOfPages<S>) -> PageRange<S> {
+    let start = range.end;
+
+    let end = start + u64::try_from(n.as_usize()).unwrap();
+
+    assert!(start <= end, "An overflow happened.");
+
+    PageRange { start, end }
+}
