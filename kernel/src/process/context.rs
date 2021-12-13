c8f8ec0ad2b8176fab1a4a51191826bcbf583f8a
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-13 17:55:15 +0900

    chore: impl `Default`

--- kernel/src/process/context.rs
+++ kernel/src/process/context.rs
@@ -10,7 +10,7 @@ use {
 };
 
 #[repr(C, align(16))]
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub(super) struct Context {
     rax: u64,
     rbx: u64,
@@ -42,7 +42,7 @@ pub(super) struct Context {
     rflags: u64,
     _fxsave_requires_16_bytes_alinged_address: u64,
 
-    fxsave_area: [u8; 512],
+    fxsave_area: FxsaveArea,
 }
 const_assert_eq!(size_of::<Context>(), 8 * 4 * 6 + 512);
 impl Context {
@@ -76,6 +76,7 @@ impl Context {
         }
     }
 
+    #[allow(clippy::too_many_arguments)]
     fn new(
         entry: VirtAddr,
         pml4: PhysFrame,
@@ -102,34 +103,12 @@ impl Context {
         }
     }
 }
-impl Default for Context {
+
+#[repr(transparent)]
+#[derive(Copy, Clone, Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
+struct FxsaveArea([u8; 512]);
+impl Default for FxsaveArea {
     fn default() -> Self {
-        Self {
-            rax: 0,
-            rbx: 0,
-            rcx: 0,
-            rdx: 0,
-            rsp: 0,
-            rbp: 0,
-            rsi: 0,
-            rdi: 0,
-            r8: 0,
-            r9: 0,
-            r10: 0,
-            r11: 0,
-            r12: 0,
-            r13: 0,
-            r14: 0,
-            r15: 0,
-            cs: 0,
-            ss: 0,
-            fs: 0,
-            gs: 0,
-            cr3: 0,
-            rip: 0,
-            rflags: 0,
-            _fxsave_requires_16_bytes_alinged_address: 0,
-            fxsave_area: [0; 512],
-        }
+        Self([0; 512])
     }
 }
