d895e7897dfb85cda5fd9156b9cde7ecc4f2cc0e
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-15 20:38:00 +0900

    chore: remove unnecessary interrupt handlers

--- kernel/src/interrupt/handler.rs
+++ kernel/src/interrupt/handler.rs
@@ -9,18 +9,6 @@ pub(super) extern "x86-interrupt" fn h_20(_: InterruptStackFrame) {
     process::switch();
 }
 
-pub(super) extern "x86-interrupt" fn h_80(_: InterruptStackFrame) {
-    let v = syscall_prepare_arguments();
-    local::end_of_interrupt();
-    process::switch();
-}
-
-pub(super) extern "x86-interrupt" fn h_81(_: InterruptStackFrame) {
-    syscall_prepare_arguments();
-    local::end_of_interrupt();
-    process::switch();
-}
-
 #[naked]
 extern "sysv64" fn syscall_prepare_arguments() -> u64 {
     unsafe {
--- kernel/src/interrupt/idt.rs
+++ kernel/src/interrupt/idt.rs
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use {
-    crate::interrupt::handler::{h_20, h_80, h_81},
-    conquer_once::spin::Lazy,
-    x86_64::{structures::idt::InterruptDescriptorTable, PrivilegeLevel},
+    crate::interrupt::handler::h_20, conquer_once::spin::Lazy,
+    x86_64::structures::idt::InterruptDescriptorTable,
 };
 
 static IDT: Lazy<InterruptDescriptorTable> = Lazy::new(|| {
@@ -11,14 +10,6 @@ static IDT: Lazy<InterruptDescriptorTable> = Lazy::new(|| {
 
     unsafe {
         idt[0x20].set_handler_fn(h_20).set_stack_index(0);
-
-        idt[0x80]
-            .set_handler_fn(h_80)
-            .set_privilege_level(PrivilegeLevel::Ring3);
-
-        idt[0x81]
-            .set_handler_fn(h_81)
-            .set_privilege_level(PrivilegeLevel::Ring3);
     }
 
     idt
