4d5317a9187308c71b0f810194e9456bb9f659e7
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-15 20:38:31 +0900

    refactor: remove an unused function

--- kernel/src/interrupt/handler.rs
+++ kernel/src/interrupt/handler.rs
@@ -8,21 +8,3 @@ pub(super) extern "x86-interrupt" fn h_20(_: InterruptStackFrame) {
     local::end_of_interrupt();
     process::switch();
 }
-
-#[naked]
-extern "sysv64" fn syscall_prepare_arguments() -> u64 {
-    unsafe {
-        asm!(
-            "
-    mov rcx, rdx
-    mov rdx, rsi
-    mov rsi, rdi
-    mov rdi, rax
-
-    call select_proper_syscall
-
-    ret",
-            options(noreturn)
-        )
-    }
-}
