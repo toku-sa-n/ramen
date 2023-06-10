81b0c71f5aed306da7ef2e8222cf21be90e199a1
Hiroki Tokunaga <tokusan441@gmail.com>
2023-06-10 13:39:24 +0900

    Remove an unused feature (#1170)

--- libs/ralib/src/lib.rs
+++ libs/ralib/src/lib.rs
@@ -2,10 +2,7 @@
 
 #![no_std]
 #![allow(clippy::too_many_arguments)] // A workaround for the clippy's wrong warning.
-#![feature(alloc_error_handler)]
 #![deny(unsafe_op_in_unsafe_fn)]
-
-use core::alloc::Layout;
 
 pub mod io;
 pub mod mem;
@@ -19,9 +16,4 @@ pub fn init() {
 #[panic_handler]
 fn panic(i: &core::panic::PanicInfo<'_>) -> ! {
     syscalls::panic(i);
-}
-
-#[alloc_error_handler]
-fn alloc_fail(l: Layout) -> ! {
-    panic!("Allocation failed: {:?}", l)
 }
